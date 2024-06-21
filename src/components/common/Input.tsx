18bd2108da6ed9b3b4ccff9198296e9a1aa04acf
knoa0405 <knoa0405@gmail.com>
2024-06-21 16:05:40 +0900

    feat: input 컴포넌트 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,49 +1,68 @@
-import { ChangeEvent } from "react";
+import * as React from "react";
 
-type TInputProps = {
-  type?: string;
-  name?: string;
-  placeholder?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  style?: string;
-};
+import { cn } from "@/utils/cn";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Field, Label } from "@headlessui/react";
 
-export default function Input({ type, name, placeholder, onChange, style }: TInputProps) {
-  return (
-    <>
-      <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        onChange={onChange}
-        className={`border-grayscale-500 rounded-lg border border-solid ${style}`}
-      />
-    </>
-  );
-}
+const inputGroupVariants = cva(
+  [
+    "flex w-full rounded-sm border px-3 py-2 text-sm",
+    "border-input bg-background ring-offset-background", // default
+    "placeholder:text-muted-foreground", // placeholder
+    "file:border-0 file:bg-transparent file:text-sm file:font-medium", // file input
+    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2", // focus
+    "disabled:cursor-not-allowed disabled:opacity-50", // disabled
+  ],
+  {
+    variants: {
+      variant: {
+        default: "", // 텍스트 버튼
+      },
+      size: {
+        default: "",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
 
-/*
-box-sizing: border-box;
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  VariantProps<typeof inputGroupVariants> & {
+    validation?: "success" | "error";
+    errorMessage?: string;
+    variant?: "default";
+    size?: "default";
+    label: string;
+  };
 
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, variant, validation, errorMessage, size, label = "아이디", ...props }, ref) => {
+    return (
+      <Field disabled={props.disabled} className={cn("relative flex w-full flex-col")}>
+        <Label
+          className={cn("primary-900", {
+            "text-muted-foreground": validation === "success",
+          })}
+          htmlFor={props.id}
+        >
+          {label}
+        </Label>
+        <input
+          id={props.id}
+          type={type}
+          className={cn(inputGroupVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        />
+        {errorMessage && <p className="text-destructive-foreground text-xs">{errorMessage}</p>}
+      </Field>
+    );
+  },
+);
 
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-padding: 16px;
-gap: 16px;
+Input.displayName = "Input";
 
-width: 386px;
-height: 56px;
-
-
-background: #FFFFFF;
-
-border: 1px solid #C5C5C5;
-border-radius: 8px;  t
-
-
-flex: none;
-order: 1;
-flex-grow: 0;
-*/
+export { Input };
