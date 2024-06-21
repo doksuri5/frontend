6f2cb17202234979ac8f921061d671302d3aaf96
knoa0405 <knoa0405@gmail.com>
2024-06-21 17:37:33 +0900

    feat: input 초기 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Input from "@/components/common/Input";
+import { Input } from "@/components/common/Input";
 
 export default function Home() {
   return (
     <>
       <h1 className="bg-background-100">Home Component</h1>
-      <Input />
+      <Input type="password" label="아이디" />
     </>
   );
 }
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,49 +1,90 @@
-import { ChangeEvent } from "react";
+"use client";
 
-type TInputProps = {
-  type?: string;
-  name?: string;
-  placeholder?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  style?: string;
-};
+import * as React from "react";
 
-export default function Input({ type, name, placeholder, onChange, style }: TInputProps) {
-  return (
-    <>
-      <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        onChange={onChange}
-        className={`border-grayscale-500 rounded-lg border border-solid ${style}`}
-      />
-    </>
-  );
-}
+import { cn } from "@/utils/cn";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Field, Label } from "@headlessui/react";
 
-/*
-box-sizing: border-box;
+import Image from "next/image";
+import EyeHideIcon from "@/app/svg/eyeHide_icon.svg";
+import EyeShowIcon from "@/app/svg/eyeShow_icon.svg";
 
+const inputGroupVariants = cva(
+  [
+    "flex w-full rounded-sm border px-3 py-2 ",
+    "border-input bg-background ring-offset-background", // default
+    "placeholder:text-muted-foreground", // placeholder
+    "file:border-0 file:bg-transparent file:text-sm file:font-medium", // file input
+    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2", // focus
+    "disabled:cursor-not-allowed disabled:opacity-50", // disabled
+  ],
+  {
+    variants: {
+      variant: {
+        default: "", // 텍스트 버튼
+      },
+      size: {
+        default: "",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
 
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-padding: 16px;
-gap: 16px;
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  VariantProps<typeof inputGroupVariants> & {
+    validation?: "success" | "error";
+    errorMessage?: string;
+    variant?: "default";
+    size?: "default";
+    label: string;
+  };
 
-width: 386px;
-height: 56px;
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, variant, validation, errorMessage, size, label, ...props }, ref) => {
+    return (
+      <div className={cn("relative flex w-full flex-col")}>
+        <label
+          className={cn("primary-900", {
+            "text-muted-foreground": validation === "success",
+          })}
+          htmlFor={props.id}
+        >
+          {label}
+        </label>
+        <input
+          id={props.id}
+          type={type}
+          className={cn(inputGroupVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            className="absolute bottom-0 right-0 top-0 flex h-10 w-10 items-center justify-center"
+            onClick={() => {
+              if (type === "password") {
+              } else {
+              }
+            }}
+          >
+            <Image
+              src={type === "password" ? EyeHideIcon : EyeShowIcon}
+              alt={type === "password" ? "비밀번호 숨기기" : "비밀번호 보이기"}
+            />
+          </button>
+        )}
+        {errorMessage && <p className="text-destructive-foreground text-xs">{errorMessage}</p>}
+      </div>
+    );
+  },
+);
 
+Input.displayName = "Input";
 
-background: #FFFFFF;
-
-border: 1px solid #C5C5C5;
-border-radius: 8px;  t
-
-
-flex: none;
-order: 1;
-flex-grow: 0;
-*/
+export { Input };
