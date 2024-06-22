f2897c08fa3237ae4d9c337a198a82473f1db743
yoonyesol <maniac970430@naver.com>
2024-06-22 21:58:43 +0900

    rename: CardSimple -> Card1컴포넌트 이름 변경

--- src/components/common/Card1.tsx
+++ src/components/common/Card1.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-type TCardSimpleProps = {
-  date: string;
-  title: string;
-  stockCode: string;
+type TCardProps = {
+  date?: string;
+  title?: string;
+  stockCode?: string;
 };
 
-export default function CardSimple({ date, title, stockCode }: TCardSimpleProps) {
+export default function Card1({ date, title, stockCode }: TCardProps) {
   return (
     <div className="flex h-[100px] w-[355px] items-center rounded-3xl border border-grayscale-200 bg-grayscale-0 px-[16px] py-[24px]">
       <div className="h-13 flex w-[323px] flex-row justify-between gap-8">
@@ -14,12 +14,16 @@ export default function CardSimple({ date, title, stockCode }: TCardSimpleProps)
           <div>{date}</div>
           <div className="body_2 truncate font-bold text-gray-900">{title}</div>
         </div>
-        <Image
-          src={`https://ssl.pstatic.net/imgstock/fn/real/logo/stock/Stock${stockCode}.O.svg`}
-          alt="logo"
-          width={48}
-          height={48}
-        />
+        {stockCode ? (
+          <Image
+            src={`https://ssl.pstatic.net/imgstock/fn/real/logo/stock/Stock${stockCode}.O.svg`}
+            alt="logo"
+            width={48}
+            height={48}
+          />
+        ) : (
+          <div className="h-[48px] w-[48px] rounded-[50%] bg-grayscale-200"></div>
+        )}
       </div>
     </div>
   );
