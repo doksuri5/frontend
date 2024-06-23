fa0ea68e6a9b305a7e0b3758a16c1e690dc22e57
knoa0405 <knoa0405@gmail.com>
2024-06-23 23:28:30 +0900

    feat: 공통 컴포넌트 input
    - 공통 컴포넌트 확장 기능
    - 디자인 시스템에 맞게 variant 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,31 +1,51 @@
 "use client";
 
-import * as React from "react";
+import React, { ElementType, forwardRef, InputHTMLAttributes, LabelHTMLAttributes, useMemo, useState } from "react";
 
 import { cn } from "@/utils/cn";
-import { cva, type VariantProps } from "class-variance-authority";
-import { Field, Label } from "@headlessui/react";
+import { cva } from "class-variance-authority";
 
-import Image from "next/image";
-import EyeHideIcon from "@/app/svg/eyeHide_icon.svg";
-import EyeShowIcon from "@/app/svg/eyeShow_icon.svg";
+import EyeHideIcon from "@/assets/icons/eye-hide-icon";
+import EyeShowIcon from "@/assets/icons/eye-show-icon";
+import Label from "./Label";
+import Caption from "./Caption";
 
-const inputGroupVariants = cva(
+export type VariantTypes = "default" | "error" | "success";
+
+export type TSizes = "default" | "sm" | "lg";
+
+export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  variant?: VariantTypes;
+  labelName?: string;
+  size?: TSizes;
+  // validation
+  errorMessage?: string;
+  // custom style class
+  inputGroupClass?: string;
+  labelClass?: string;
+  inputClass?: string;
+  captionClass?: string;
+  // suffix icon or button
+  suffix?: ElementType;
+};
+
+const inputVariants = cva(
   [
-    "flex w-full rounded-sm border px-3 py-2 ",
-    "border-input bg-background ring-offset-background", // default
-    "placeholder:text-muted-foreground", // placeholder
-    "file:border-0 file:bg-transparent file:text-sm file:font-medium", // file input
-    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2", // focus
-    "disabled:cursor-not-allowed disabled:opacity-50", // disabled
+    "w-full absolute", // block
+    "border rounded-lg", // border
+    "disabled:bg-grayscale-100 disabled:text-grayscale-300 disabled:placeholder-grayscale-300", // disabled
   ],
   {
     variants: {
       variant: {
-        default: "", // 텍스트 버튼
+        default: "border-grayscale-300 focus:outline-blue-500",
+        error: "border-warning-100 focus:outline-warning-100",
+        success: "border-grayscale-300 focus:outline-blue-500",
       },
       size: {
-        default: "",
+        default: "p-4 body_4",
+        sm: "p-2 body_5",
+        lg: "p-6 body_3",
       },
     },
     defaultVariants: {
@@ -35,52 +55,83 @@ const inputGroupVariants = cva(
   },
 );
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
-  VariantProps<typeof inputGroupVariants> & {
-    validation?: "success" | "error";
-    errorMessage?: string;
-    variant?: "default";
-    size?: "default";
-    label: string;
-  };
+const trailingButtonVariants = cva("absolute", {
+  variants: {
+    variant: {
+      default: "text-blue-500",
+      error: "text-warning-100",
+      success: "text-success-100",
+    },
+    size: {
+      default: "right-4 top-4",
+      sm: "right-2 top-2",
+      lg: "right-6 top-6",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, variant, validation, errorMessage, size, label, ...props }, ref) => {
+const InputGroup = ({ className, children }: { className?: string; children: React.ReactNode }) => {
+  return <div className={cn("flex flex-col", className)}>{children}</div>;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      type,
+      variant,
+      errorMessage,
+      size,
+      labelName,
+      inputGroupClass,
+      labelClass,
+      inputClass,
+      captionClass,
+      suffix: SuffixComp,
+      ...props
+    },
+    ref,
+  ) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const handlePasswordVisibility = () => {
+      setIsPasswordVisible((prev) => !prev);
+    };
+
+    const inputType = useMemo(
+      () => (type === "password" && isPasswordVisible ? "text" : type),
+      [type, isPasswordVisible],
+    );
+
     return (
-      <div className={cn("relative flex w-full flex-col")}>
-        <label
-          className={cn("primary-900", {
-            "text-muted-foreground": validation === "success",
-          })}
-          htmlFor={props.id}
-        >
-          {label}
-        </label>
-        <input
-          id={props.id}
-          type={type}
-          className={cn(inputGroupVariants({ variant, size, className }))}
-          ref={ref}
-          {...props}
-        />
-        {type === "password" && (
-          <button
-            type="button"
-            className="absolute bottom-0 right-0 top-0 flex h-10 w-10 items-center justify-center"
-            onClick={() => {
-              if (type === "password") {
-              } else {
-              }
-            }}
-          >
-            <Image
-              src={type === "password" ? EyeHideIcon : EyeShowIcon}
-              alt={type === "password" ? "비밀번호 숨기기" : "비밀번호 보이기"}
-            />
-          </button>
-        )}
-        {errorMessage && <p className="text-destructive-foreground text-xs">{errorMessage}</p>}
-      </div>
+      <InputGroup className={inputGroupClass}>
+        <Label labelName={labelName} htmlFor={props.id} variant={variant} size={size} labelClass={labelClass}>
+          <input
+            type={inputType}
+            id={props.id}
+            className={cn(inputVariants({ variant, size, className: inputClass }))}
+            ref={ref}
+            {...props}
+          />
+          {type === "password" && (
+            <button
+              type="button"
+              className={cn(trailingButtonVariants({ variant, size }))}
+              onClick={handlePasswordVisibility}
+            >
+              {isPasswordVisible ? (
+                <EyeShowIcon color={variant === "error" ? "#FF294F" : undefined} />
+              ) : (
+                <EyeHideIcon color={variant === "error" ? "#FF294F" : undefined} />
+              )}
+            </button>
+          )}
+          {SuffixComp && <SuffixComp />}
+        </Label>
+        {errorMessage && <Caption caption={errorMessage} variant={variant} captionClass={captionClass} />}
+      </InputGroup>
     );
   },
 );
