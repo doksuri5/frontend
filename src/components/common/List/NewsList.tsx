5f84d8b5aa1ce61df6229ecc9eef2138a08875b8
ono212 <qkrwjdals302@naver.com>
2024-06-24 10:29:12 +0900

    feat: NewsItem의 리스트인 NewsList컴포넌트 구현

---
+++ src/components/common/List/NewsList.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import NewsItem, { TINewsItemProps } from "./NewsItem";
+import { Fragment } from "react";
+
+type TINewsListProps = {
+  newsItemArray: TINewsItemProps[];
+  variant?: "border" | "noBorder";
+  style?: string;
+};
+
+const variantStyles = {
+  border: {
+    border: "rounded-[32px] border border-solid border-navy-100 p-12",
+  },
+  noBorder: { border: "" },
+};
+
+const Divider = () => {
+  return <hr className="my-[32px] border border-solid border-grayscale-400" />;
+};
+
+export default function NewsList({ newsItemArray, variant = "border", style }: TINewsListProps) {
+  const selectedVariantStyles = variantStyles[variant];
+
+  return (
+    <div className={`flex w-full flex-col ${selectedVariantStyles.border} ${style}`}>
+      {newsItemArray.map((newsItem, index) => (
+        <Fragment key={index}>
+          <NewsItem
+            image={newsItem.image}
+            title={newsItem.title}
+            description={newsItem.description}
+            publishedTime={newsItem.publishedTime}
+            newspaperCompany={newsItem.newspaperCompany}
+            style={newsItem?.style}
+            variant="lineBreak4"
+          />
+          {index < newsItemArray.length - 1 && <Divider />}
+        </Fragment>
+      ))}
+    </div>
+  );
+}
