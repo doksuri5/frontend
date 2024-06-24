5e612857a9a6455026cf9b6a79ac12db11279a84
yoonyesol <maniac970430@naver.com>
2024-06-24 16:11:13 +0900

    refactor: card 이름 지정 및 next.config 파일 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["imgnews.pstatic.net"],
+    remotePatterns: [{ protocol: "https", hostname: "imgnews.pstatic.net" }],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
 };
 
