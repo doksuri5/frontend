65c44330ca0328c0b82db97cbec607e66e190a3d
magrowing <kmagrowing@gmail.com>
2024-06-26 11:16:46 +0900

    feat : 공통 컴포넌트 ChechkBox 추가

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -5,31 +5,49 @@
  * setChecked → checked 변경 함수
  */
 
-import { Checkbox, Field, Label } from "@headlessui/react";
+import { InputHTMLAttributes } from "react";
+
+import { Checkbox } from "@headlessui/react";
 
 import Image from "next/image";
 
 import { cn } from "@/utils/cn";
 
 type TCheckProps = {
+  variants?: string;
   label?: string;
+  className?: string;
   checked: boolean;
   setChecked: (checked: boolean) => void;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export default function CheckBox({ label, checked, setChecked }: TCheckProps) {
+export default function CheckBox({
+  variants = "default",
+  label,
+  checked,
+  setChecked,
+  className,
+  ...props
+}: TCheckProps) {
   return (
-    <Field>
-      <Checkbox checked={checked} onChange={setChecked} className="flex_row">
-        <Image
-          src={checked ? "/icons/checkbox_select.svg" : "/icons/checkbox_default.svg"}
-          alt={"체크박스 아이콘"}
-          width={24}
-          height={24}
-          className="overflow:hidden"
-        />
-        {label && <Label className={cn("body_5 ml-[0.4rem] text-grayscale-900")}>{label}</Label>}
-      </Checkbox>
-    </Field>
+    <Checkbox checked={checked} onChange={setChecked} className={cn("flex_row", className)}>
+      {variants === "radio" && (
+        <label className={cn("body_3 text-grayscale-900")} htmlFor={props.id}>
+          {label}
+        </label>
+      )}
+      <Image
+        src={checked ? `/icons/checkbox_${variants}_on.svg` : `/icons/checkbox_${variants}.svg`}
+        alt={"체크박스 아이콘"}
+        width={24}
+        height={24}
+      />
+      <input type="checkbox" id={props.id} checked={checked} className="hidden" readOnly {...props} />
+      {variants === "default" && (
+        <label className={cn("body_5 ml-[0.4rem] text-grayscale-900")} htmlFor={props.id}>
+          {label}
+        </label>
+      )}
+    </Checkbox>
   );
 }
