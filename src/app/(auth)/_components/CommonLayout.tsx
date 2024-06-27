766181d56aa031080f99aed456d43ea95e4b51a1
magrowing <kmagrowing@gmail.com>
2024-06-27 23:25:35 +0900

    refactor : 회원탈퇴완료 UI 수정

--- src/app/(auth)/_components/CommonLayout.tsx
+++ src/app/(auth)/_components/CommonLayout.tsx
@@ -3,16 +3,17 @@ import CommonTitle, { TCommonTitleProps } from "./CommonTitle";
 import { cn } from "@/utils/cn";
 
 type TCommonLayout = TCommonTitleProps & {
+  childrenClass?: string;
   children: React.ReactNode;
 };
 
-export default function CommonLayout({ title, desc, children }: TCommonLayout) {
+export default function CommonLayout({ title, desc, childrenClass, children }: TCommonLayout) {
   return (
     <>
       <article className="flex_col_center pb-[10rem] pt-[10rem]">
-        <section className={cn("w-[59rem] rounded-[3.2rem] bg-grayscale-0 px-[10.2rem] py-[8rem]")}>
+        <section className={cn("min-w-[59rem] rounded-[3.2rem] bg-grayscale-0 px-[10.2rem] py-[8rem]")}>
           <CommonTitle title={title} desc={desc} />
-          <div className="w-[38.6rem]">{children}</div>
+          <div className={cn("w-[38.6rem]", childrenClass)}>{children}</div>
         </section>
       </article>
     </>
--- src/app/(auth)/withdraw/page.tsx
+++ src/app/(auth)/withdraw/page.tsx
@@ -5,13 +5,13 @@ import { Button } from "@/components/common";
 export default function page() {
   return (
     <>
-      <CommonLayout title="회원탈퇴가 완료되었습니다.">
+      <CommonLayout title="회원탈퇴가 완료되었습니다." childrenClass="w-[39.4rem] flex_col_center">
         <p className="body_2 text-center text-grayscale-900">
           아잇나우를 이용해주셔서 감사합니다.
           <br />
           더욱 더 노력하고 발전하는 아잇나우가 되겠습니다.
         </p>
-        <Button size="lg" className="mt-[5.6rem] text-white">
+        <Button size="lg" className="mt-[5.6rem] w-[36.8rem] text-white">
           확인
         </Button>
       </CommonLayout>
