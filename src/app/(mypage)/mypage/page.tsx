fab133fbb2e8d9ff9688f16904af4b8811fba290
yoonyesol <maniac970430@naver.com>
2024-06-27 17:25:52 +0900

    feat: 마이페이지 공통 layout 작성 및 언어 설정 페이지 UI 작성

---
+++ src/app/(mypage)/mypage/page.tsx
@@ -0,0 +1,29 @@
+export default function MyPage() {
+  return (
+    <div className="">
+      <div>
+        <div className="flex flex-col gap-[.8rem]">
+          <div className="body_2 font-bold text-gray-900">프로필 설정</div>
+          <div>서비스 사용시 보여지는 프로필을 생성 및 변경합니다. 프로필을 설정해보세요.</div>
+        </div>
+      </div>
+      <div></div>
+      <div className="flex flex-col gap-[2.4rem]">
+        <div>계정 설정</div>
+        <div>서비스 이용시 사용되는 계정을 생성 및 변경합니다. 계정을 연동하여 다양한 서비스를 이용해보세요.</div>
+      </div>
+      <div>
+        <div>아이디</div>
+        <div>sfacspaceid</div>
+      </div>
+      <div>
+        <div>이름</div>
+        <div>김스팩</div>
+      </div>
+      <div>
+        <div>생년월일</div>
+        <div>991231</div>
+      </div>
+    </div>
+  );
+}
