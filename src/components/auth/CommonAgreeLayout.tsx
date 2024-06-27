321dfb8f6bab394012752fffc1a0a521c81de9b9
magrowing <kmagrowing@gmail.com>
2024-06-27 11:52:08 +0900

    refactor : scrollbar class명 변경

--- src/components/auth/CommonAgreeLayout.tsx
+++ src/components/auth/CommonAgreeLayout.tsx
@@ -10,7 +10,9 @@ export default function CommonAgreeLayout({ title, children }: TCommonAgreeLayou
     <div>
       <h3 className="body_3 mb-[0.8rem] text-grayscale-900">{title}</h3>
       <div className={cn("mb-[.8rem] box-border w-full rounded-[.8rem] border border-grayscale-300 py-[1.6rem]")}>
-        <div className="body_4 scrollbar-thin scrollbar h-[16rem] w-full overflow-y-scroll px-[1.6rem]">{children}</div>
+        <div className="body_4 scrollbar-thin scrollbar_style h-[16rem] w-full overflow-y-scroll px-[1.6rem]">
+          {children}
+        </div>
       </div>
     </div>
   );
