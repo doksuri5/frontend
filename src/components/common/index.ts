bc439ffba5dd6399e6731920c05fd4601829bc0c
윤태현 <yoonth0919@gmail.com>
2024-06-28 21:55:17 +0900

    feat: Alert 컴포넌트 생성

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -14,6 +14,7 @@ import Modal from "./Modal";
 import { Skeleton } from "./Skeleton";
 import FindNewsSkeleton from "./List/skeleton/FindNewsSkeleton";
 import StockItemSkeleton from "./List/skeleton/StockItemSkeleton";
+import Alert from "./Alert";
 
 export {
   Button,
@@ -32,4 +33,5 @@ export {
   Skeleton,
   FindNewsSkeleton,
   StockItemSkeleton,
+  Alert,
 };
