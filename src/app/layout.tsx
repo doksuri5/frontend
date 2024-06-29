154fa9854b8054d01aac40330e0530e1dcd13c99
ono212 <qkrwjdals302@naver.com>
2024-06-30 00:23:16 +0900

    chore: tanstack-query 설치 및 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { pretendard } from "@/fonts";
 
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import QueryProvider from "@/providers/QueryProvider";
 
 export default function RootLayout({
   children,
@@ -12,7 +13,9 @@ export default function RootLayout({
     <html lang="ko" className={pretendard.className}>
       <body className="h-[calc(100vh-8rem)] bg-background-100">
         <Header />
-        <main className="relative top-[8rem] m-auto min-h-full max-w-[120rem]">{children}</main>
+        <main className="relative top-[8rem] m-auto min-h-full max-w-[120rem]">
+          <QueryProvider>{children}</QueryProvider>
+        </main>
       </body>
     </html>
   );
