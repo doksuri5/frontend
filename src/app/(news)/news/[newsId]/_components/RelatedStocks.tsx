1b55226dd23b83154f6f4fa6b4f66870e0a27991
ono212 <qkrwjdals302@naver.com>
2024-06-30 03:04:57 +0900

    feat: StockItem 사이에 gap 추가

--- src/app/(news)/news/[newsId]/_components/RelatedStocks.tsx
+++ src/app/(news)/news/[newsId]/_components/RelatedStocks.tsx
@@ -8,18 +8,20 @@ export default function RelatedStocks({ stockData }: TRelatedStocksProps) {
   return (
     <section className="min-h-[31rem] min-w-[38.4rem] rounded-[1.6rem] bg-white p-[3.2rem]">
       <h2 className="body_3 pb-[1rem] font-bold text-navy-900">현재 뉴스와 관련된 주식</h2>
-      {stockData.map((stock) => (
-        <StockItem
-          variant="stock"
-          icon={stock.icon}
-          stockKorName={stock.stockKorName}
-          stockEngName={stock.stockEngName}
-          currentPrice={stock.currentPrice}
-          fluctuationPrice={stock.fluctuationPrice}
-          fluctuationRatio={stock.fluctuationRatio}
-          key={stock.id}
-        />
-      ))}
+      <div className="flex flex-col gap-[0.8rem]">
+        {stockData.map((stock) => (
+          <StockItem
+            variant="findStock"
+            icon={stock.icon}
+            stockKorName={stock.stockKorName}
+            stockEngName={stock.stockEngName}
+            currentPrice={stock.currentPrice}
+            fluctuationPrice={stock.fluctuationPrice}
+            fluctuationRatio={stock.fluctuationRatio}
+            key={stock.id}
+          />
+        ))}
+      </div>
     </section>
   );
 }
