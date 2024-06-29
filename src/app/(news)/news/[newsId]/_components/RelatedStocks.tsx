9c78b55d5b181c91799e60af7137b2111e1c3f9f
ono212 <qkrwjdals302@naver.com>
2024-06-30 02:51:02 +0900

    feat: 관련 주식 부분 컴포넌트 구현

---
+++ src/app/(news)/news/[newsId]/_components/RelatedStocks.tsx
@@ -0,0 +1,25 @@
+import StockItem from "@/components/common/List/StockItem";
+
+type TRelatedStocksProps = {
+  stockData: any[];
+};
+
+export default function RelatedStocks({ stockData }: TRelatedStocksProps) {
+  return (
+    <section className="min-h-[31rem] min-w-[38.4rem] rounded-[1.6rem] bg-white p-[3.2rem]">
+      <h2 className="body_3 pb-[1rem] font-bold text-navy-900">현재 뉴스와 관련된 주식</h2>
+      {stockData.map((stock) => (
+        <StockItem
+          variant="stock"
+          icon={stock.icon}
+          stockKorName={stock.stockKorName}
+          stockEngName={stock.stockEngName}
+          currentPrice={stock.currentPrice}
+          fluctuationPrice={stock.fluctuationPrice}
+          fluctuationRatio={stock.fluctuationRatio}
+          key={stock.id}
+        />
+      ))}
+    </section>
+  );
+}
