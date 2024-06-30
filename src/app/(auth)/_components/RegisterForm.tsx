e8129b84744365856e5eb072893ec740ccfae3c7
magrowing <kmagrowing@gmail.com>
2024-07-01 00:36:18 +0900

    feat :본인인증/회원가입/프로필 유효성 검사

--- src/app/(auth)/_components/RegisterForm.tsx
+++ src/app/(auth)/_components/RegisterForm.tsx
@@ -6,15 +6,24 @@ import Select, { components } from "react-select";
 
 import Image from "next/image";
 
-import Terms from "./Terms";
-import Privacy from "./Privacy";
-import { Input, Button, CheckBox } from "@/components/common";
+import { Input, Button } from "@/components/common";
+
+import useZodSchemaForm from "@/hooks/useZodSchemaForm";
 
 import { cn } from "@/utils/cn";
 
 import EditIcon from "@/public/icons/avatar_edit.svg?component";
 
-import { AGREE_PATH, PROFILE_SETUP_PATH, REGISTER_COMPLETE_PATH, REGISTER_PATH, VERIFY_USER_PATH } from "@/routes/path";
+import { PROFILE_SETUP_PATH, REGISTER_COMPLETE_PATH, REGISTER_PATH, VERIFY_USER_PATH } from "@/routes/path";
+
+import {
+  TProfileSchema,
+  TRegisterSchemaType,
+  TVerifyUserSchema,
+  profileSchema,
+  registerSchema,
+  verifyUserSchema,
+} from "@/types/AuthType";
 
 const options = [
   { value: "tsla", label: "# 테슬라 ∙ TSLA" },
@@ -26,35 +35,12 @@ const options = [
 ];
 
 export default function RegisterForm() {
-  const [agreedAll, setAgreedAll] = useState(false);
-  const [terms, setTerms] = useState(false);
-  const [privacy, setPrivacy] = useState(false);
-  const router = useRouter();
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
+  const [isGender, setIsGender] = useState<undefined | "M" | "F">(undefined);
   const [file, setFile] = useState("/icons/avatar_default.svg");
 
+  const router = useRouter();
   const pathname = usePathname();
-
-  const agreeAllChangeHandler = (checked: boolean) => {
-    setAgreedAll(checked);
-    setTerms(checked);
-    setPrivacy(checked);
-  };
-
-  const individualChangeHandler = (setter: React.Dispatch<React.SetStateAction<boolean>>) => (checked: boolean) => {
-    setter(checked);
-
-    if (!checked) {
-      setAgreedAll(false);
-    } else {
-      if (terms && privacy) {
-        setAgreedAll(true);
-      } else if (setter === setTerms && privacy) {
-        setAgreedAll(true);
-      } else if (setter === setPrivacy && terms) {
-        setAgreedAll(true);
-      }
-    }
-  };
 
   const avatarChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
@@ -78,68 +64,99 @@ export default function RegisterForm() {
     return "hidden";
   };
 
+  const isGenderActive = (value: undefined | "M" | "F") => {
+    setIsGender(value);
+  };
+
+  const {
+    control: verifyControl,
+    handleSubmit: handleVerifySubmit,
+    formState: { errors: verifyErrors, isValid: isVerifyValid },
+    trigger: triggerVerify,
+    watch: watchVerify,
+  } = useZodSchemaForm<TVerifyUserSchema>(verifyUserSchema);
+
+  const {
+    control: registerControl,
+    handleSubmit: handleRegisterSubmit,
+    formState: { errors: registerErrors, isValid: isRegisterValid },
+    watch: watchRegister,
+  } = useZodSchemaForm<TRegisterSchemaType>(registerSchema);
+
+  const {
+    control: profileControl,
+    handleSubmit: handleProfileSubmit,
+    formState: { errors: profileErrors, isValid: isProfileValid },
+    watch: watchProfile,
+  } = useZodSchemaForm<TProfileSchema>(profileSchema);
+
+  const emailVerificationHandler = async () => {
+    const valid = await triggerVerify("email");
+    if (valid) {
+      setIsEmailVerified(true);
+    }
+  };
+
   return (
     <>
-      {/* 약관동의  */}
-      <div className={cn("auth_form_layout", getVisibilityClass(AGREE_PATH))}>
-        <div className="border-b-[.1rem] border-grayscale-300 pb-[1.6rem]">
-          <CheckBox
-            checked={agreedAll}
-            setChecked={agreeAllChangeHandler}
-            label="이용악관, 개인정보 처리방침에 모두 동의합니다."
-            id="agreedAll"
-            name="agreedAll"
-            variants="radio"
-            className="w-full justify-between"
-            labelClass="body_3"
-          />
-        </div>
-        <div>
-          <Terms />
-          <CheckBox
-            checked={terms}
-            setChecked={individualChangeHandler(setTerms)}
-            label="동의합니다."
-            id="terms"
-            name="terms"
-            variants="radio"
-            className="w-full justify-end"
-          />
-        </div>
-        <div>
-          <Privacy />
-          <CheckBox
-            checked={privacy}
-            setChecked={individualChangeHandler(setPrivacy)}
-            label="동의합니다."
-            id="privacy"
-            name="privacy"
-            variants="radio"
-            className="w-full justify-end"
-          />
-        </div>
-        <Button
-          type="button"
-          size="lg"
-          bgColor={`${agreedAll ? "bg-navy-900" : "bg-grayscale-200"}`}
-          disabled={!agreedAll}
-          className={`${agreedAll ? "text-grayscale-0" : "text-grayscale-300"} mt-[4rem]`}
-          onClick={() => nextPage(VERIFY_USER_PATH)}
-        >
-          다음
-        </Button>
-      </div>
-
       {/* 본인인증 */}
-      <div className={cn("auth_form_layout", getVisibilityClass(VERIFY_USER_PATH))}>
-        <Input id="name" name="name" labelName="이름" placeholder="이름을 입력해주세요." />
-        <Input id="email" name="email" labelName="이메일 주소" placeholder="이메일 주소를 입력해주세요." />
+      <div className={cn("flex flex-col gap-[1.6rem]", getVisibilityClass(VERIFY_USER_PATH))}>
+        <Input id="name" labelName="이름" placeholder="이름을 입력해주세요." {...verifyControl.register("name")} />
+        <div>
+          <Input
+            id="email"
+            labelName="이메일 주소"
+            placeholder="이메일 주소를 입력해주세요."
+            variant={verifyErrors.email ? "error" : "default"}
+            caption={verifyErrors.email?.message}
+            {...verifyControl.register("email")}
+            suffix={
+              <Button
+                variant="textButton"
+                size="sm"
+                bgColor={!verifyErrors.email && watchVerify("email") ? "bg-navy-900" : "bg-grayscale-200"}
+                className={cn(
+                  `w-[12rem] ${!verifyErrors.email && watchVerify("email") ? "text-white" : "text-gray-300"}`,
+                )}
+                disabled={!watchVerify("email")}
+                onClick={emailVerificationHandler}
+              >
+                이메일 인증
+              </Button>
+            }
+          />
+          {isEmailVerified && (
+            <Input
+              id="emailCertification"
+              placeholder="이메일 인증 코드 6자리 입력"
+              {...verifyControl.register("emailCertification")}
+              inputGroupClass="mt-[.8rem]"
+              variant={verifyErrors.emailCertification ? "error" : "default"}
+              // suffix={
+              //   <Button
+              //     variant="textButton"
+              //     size="sm"
+              //     bgColor={
+              //       !verifyErrors.emailCertification && watchVerify("emailCertification") ? "bg-navy-900" : "bg-grayscale-200"
+              //     }
+              //     className={cn(
+              //       `w-[12rem] ${!verifyErrors.emailCertification && watchVerify("emailCertification") ? "text-white" : "text-gray-300"}`,
+              //     )}
+              //     disabled={!watchVerify("emailCertification")}
+              //   >
+              //     확인
+              //   </Button>
+              // }
+            />
+          )}
+        </div>
+
         <Button
           type="button"
           size="lg"
-          bgColor={`${agreedAll ? "bg-navy-900" : "bg-grayscale-200"}`}
-          disabled={!agreedAll}
-          className={`${agreedAll ? "text-grayscale-0" : "text-grayscale-300"} mt-[4rem]`}
+          bgColor={isVerifyValid ? "bg-navy-900" : "bg-grayscale-200"}
+          className={cn(`mt-[4rem] ${isVerifyValid ? "text-white" : "text-gray-300"}`)}
+          disabled={!isVerifyValid}
           onClick={() => nextPage(REGISTER_PATH)}
         >
           다음
@@ -147,66 +164,70 @@ export default function RegisterForm() {
       </div>
 
       {/* 회원가입 */}
-      <div className={cn("auth_form_layout", getVisibilityClass(REGISTER_PATH))}>
+      <div className={cn("flex flex-col gap-[1.6rem]", getVisibilityClass(REGISTER_PATH))}>
         <div>
           <Input
             id="id"
-            name="id"
             labelName="아이디"
             placeholder="아이디를 입력해주세요."
+            {...registerControl.register("id")}
+            variant={registerErrors.id ? "error" : "default"}
             caption="*  6~12자의 영문, 숫자, _,을 이용한 조합"
-            captionClass="text-grayscale-700"
             suffix={
-              <Button variant="textButton" size="sm" bgColor="bg-navy-900" className="w-[12rem]">
+              <Button
+                variant="textButton"
+                size="sm"
+                bgColor={!registerErrors.id && watchRegister("id") ? "bg-navy-900" : "bg-grayscale-200"}
+                className={cn(
+                  `w-[12rem] ${!registerErrors.id && watchRegister("id") ? "text-white" : "text-gray-300"}`,
+                )}
+                disabled={!watchRegister("id")}
+              >
                 중복 확인
               </Button>
             }
           />
-          {/* <Input
-            id="email_certification"
-            name="email_certification"
-            // labelName="인증 코드"
-            placeholder="인증 코드를 입력해주세요."
-            inputGroupClass="mt-[0.8rem] hidden"
-          /> */}
         </div>
         <Input
           type="password"
           id="password"
-          name="password"
           labelName="비밀번호 입력"
           placeholder="비밀번호를 입력해주세요."
-          captionClass="text-grayscale-700"
           caption="*  8-20자 이내 숫자, 특수문자, 영문자 중 2가지 이상을 조합"
+          {...registerControl.register("password")}
+          variant={registerErrors.password ? "error" : "default"}
         />
         <Input
           type="password"
           id="passwordChk"
-          name="passwordChk"
           labelName="비밀번호 확인"
           placeholder="비밀번호를 다시 한번 입력해주세요."
+          {...registerControl.register("passwordChk")}
+          variant={registerErrors.passwordChk ? "error" : "default"}
+          caption={registerErrors.passwordChk?.message}
         />
         <div>
-          <Input id="phone" name="phone" labelName="휴대폰번호" placeholder="-를 제외한 휴대폰번호를 입력해주세요." />
-          {/* <Input
-            id="phone_certification"
-            name="phone_certification"
-            // labelName="인증 코드"
-            placeholder="인증 코드를 입력해주세요."
-            inputGroupClass="mt-[0.8rem] hidden"
-          /> */}
+          <Input
+            id="phone"
+            labelName="휴대폰번호"
+            placeholder="-를 제외한 휴대폰번호를 입력해주세요."
+            {...registerControl.register("phone")}
+            variant={registerErrors.phone ? "error" : "default"}
+          />
         </div>
         <Input
           id="birth"
-          name="birth"
           labelName="생년월일"
           placeholder="생년월일 6자리를 입력해주세요.(예시 : 991231)"
+          {...registerControl.register("birth")}
+          variant={registerErrors.birth ? "error" : "default"}
         />
         <Button
           type="button"
           size="lg"
-          bgColor="bg-navy-900"
-          className="mt-[4rem]"
+          bgColor={isRegisterValid ? "bg-navy-900" : "bg-grayscale-200"}
+          className={cn(`mt-[4rem] ${isRegisterValid ? "text-white" : "text-gray-300"}`)}
+          disabled={!isRegisterValid}
           onClick={() => nextPage(PROFILE_SETUP_PATH)}
         >
           다음
@@ -216,7 +237,7 @@ export default function RegisterForm() {
       {/* 프로필 */}
       <div className={cn(getVisibilityClass(PROFILE_SETUP_PATH))}>
         <div className="flex_col_center relative mx-[auto] mb-[2.4rem] h-[12rem] w-[12rem]">
-          <Image src={file} width={100} height={100} alt="프로필 이미지" />
+          <Image src={file} width={100} height={100} alt="프로필 이미지" priority />
           <input type="file" accept="image/*" id="file" name="file" className="hidden" onChange={avatarChangeHandler} />
           <label htmlFor="file" className="absolute bottom-[0] right-0 h-[4rem] w-[4rem] cursor-pointer">
             <EditIcon />
@@ -224,11 +245,19 @@ export default function RegisterForm() {
         </div>
         <Input
           id="nickname"
-          name="nickname"
           labelName="닉네임"
           placeholder="닉네임을 입력해주세요."
+          {...profileControl.register("nickname")}
           suffix={
-            <Button variant="textButton" size="sm" bgColor="bg-navy-900" className="w-[12rem]">
+            <Button
+              variant="textButton"
+              size="sm"
+              bgColor={!profileErrors.nickname && watchProfile("nickname") ? "bg-navy-900" : "bg-grayscale-200"}
+              className={cn(
+                `w-[12rem] ${!profileErrors.nickname && watchProfile("nickname") ? "text-white" : "text-gray-300"}`,
+              )}
+              disabled={!watchProfile("nickname")}
+            >
               중복 확인
             </Button>
           }
@@ -254,10 +283,24 @@ export default function RegisterForm() {
         <div className="mt-[1.6rem]">
           <p className="body-4 text-navy-900">성별</p>
           <p className="flex_row gap-[.8rem]">
-            <Button type="button" variant="textButton" size="md" bgColor="bg-white">
+            <Button
+              type="button"
+              variant="textButton"
+              size="md"
+              bgColor={isGender === "M" ? "bg-navy-900" : "bg-white"}
+              value="M"
+              onClick={() => isGenderActive("M")}
+            >
               남성
             </Button>
-            <Button type="button" variant="textButton" size="md" bgColor="bg-navy-900" className="">
+            <Button
+              type="button"
+              variant="textButton"
+              size="md"
+              bgColor={isGender === "F" ? "bg-navy-900" : "bg-white"}
+              value="F"
+              onClick={() => isGenderActive("F")}
+            >
               여성
             </Button>
           </p>
@@ -266,8 +309,9 @@ export default function RegisterForm() {
           type="button"
           variant="textButton"
           size="lg"
-          bgColor="bg-navy-900"
-          className="mt-[5.6rem]"
+          bgColor={isProfileValid ? "bg-navy-900" : "bg-grayscale-200"}
+          className={cn(`mt-[4rem] ${isProfileValid ? "text-white" : "text-gray-300"}`)}
+          disabled={!isProfileValid}
           onClick={() => nextPage(REGISTER_COMPLETE_PATH)}
         >
           가입하기
--- src/types/AuthType.ts
+++ src/types/AuthType.ts
@@ -19,6 +19,46 @@ export const findPasswordSchema = z.object({
   email: z.string().email({ message: "올바른 이메일 형식이 아닙니다." }),
 });
 
+// 본인인증
+export const verifyUserSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email({ message: "올바른 이메일 형식이 아닙니다." }),
+  emailCertification: z.string().min(6).max(6).regex(/^\d+$/, { message: "인증코드가 일치하지 않습니다." }),
+});
+
+// 회원가입
+export const registerSchema = z
+  .object({
+    id: z
+      .string()
+      .min(6, { message: "아이디는 최소 6자 이상이어야 합니다." })
+      .max(12, { message: "아이디는 최대 12자 이내여야 합니다." })
+      .regex(/^[a-zA-Z0-9_]+$/, { message: "아이디는 영문, 숫자, _, 만 사용할 수 있습니다." }),
+    password: z.string().regex(/^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\[\]{};':",.<>\/?\-]).{8,20}$/, {
+      message: "8-20자 이내 숫자, 특수문자, 영문자 중 2가지 이상을 조합",
+    }),
+    passwordChk: z.string().regex(/^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\[\]{};':",.<>\/?\-]).{8,20}$/, {
+      message: "8-20자 이내 숫자, 특수문자, 영문자 중 2가지 이상을 조합",
+    }),
+    phone: z.string().min(10).max(12).regex(/^\d+$/),
+    birth: z
+      .string()
+      .length(6, { message: "생년월일은 6자리여야 합니다." })
+      .regex(/^\d{6}$/, { message: "유효한 생년월일을 입력해주세요." }),
+  })
+  .refine((data) => data.password === data.passwordChk, {
+    message: "동일한 비밀번호가 아닙니다. 다시 확인 후 입력해주세요.",
+    path: ["passwordChk"],
+  });
+
+// 프로필
+export const profileSchema = z.object({
+  nickname: z.string().min(1),
+});
+
 export type TLoginSchema = z.infer<typeof loginSchema>;
 export type TFindEmailSchema = z.infer<typeof findIdSchema>;
 export type TFindPasswordSchema = z.infer<typeof findPasswordSchema>;
+export type TVerifyUserSchema = z.infer<typeof verifyUserSchema>;
+export type TRegisterSchemaType = z.infer<typeof registerSchema>;
+export type TProfileSchema = z.infer<typeof profileSchema>;
