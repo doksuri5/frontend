e9093d0aa820984be5c27921cb70ea8428c8bf36
knoa0405 <knoa0405@gmail.com>
2024-06-30 17:34:45 +0900

    feat: 루트 레이아웃에 챗봇 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { pretendard } from "@/fonts";
 
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import { ChatBot } from "@/components/common";
 
 export default function RootLayout({
   children,
@@ -13,6 +14,7 @@ export default function RootLayout({
       <body className="h-[calc(100vh-8rem)]">
         <Header />
         <main className="relative top-[8rem] m-auto min-h-full w-full bg-background-100 px-[12rem]">{children}</main>
+        <ChatBot />
       </body>
     </html>
   );
