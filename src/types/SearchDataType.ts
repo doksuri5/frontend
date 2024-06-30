472a760108315534dd8f948b050d214b74b0f719
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-06-30 17:11:09 +0900

    feat: 공통으로 사용되는 인기 검색어, 최근 검색어 타입 추가

--- src/types/SearchDataType.ts
+++ src/types/SearchDataType.ts
@@ -1,4 +1,14 @@
+import { StockDataType } from "./StockDataType";
+
 export interface SearchDataType {
+  _id: string;
+  user_id: string;
+  stockName: string;
+  symbolCode: string;
+  created_at: string; // 추후에 date 값으로 변경 예정
+}
+
+export interface SearchDetailDataType extends StockDataType {
   _id: string;
   user_id: string;
   stockName: string;
