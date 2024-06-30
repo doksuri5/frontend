d29858901c5440432e7fdf77296c65d4fbf39d9c
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-06-30 17:45:41 +0900

    feat: 최근 검색어 중복 제거 및 최신 값을 맨 앞으로

--- src/stores/useRecentSearchStore.ts
+++ src/stores/useRecentSearchStore.ts
@@ -10,7 +10,21 @@ interface RecentSearchType {
 const useRecentSearchStore = create<RecentSearchType>()((set) => ({
   stockItemList: [],
   setStockItemList: (fetchData) => set({ stockItemList: fetchData }),
-  addStockItemList: (newStockItem) => set((state) => ({ stockItemList: [...state.stockItemList, newStockItem] })),
+  addStockItemList: (newStockItem) =>
+    set((state) => {
+      const existingIndex = state.stockItemList.findIndex((item) => item.stockName === newStockItem.stockName);
+
+      if (existingIndex !== -1) {
+        // 이미 존재하는 항목을 배열에서 제거
+        const updatedList = [...state.stockItemList];
+        updatedList.splice(existingIndex, 1);
+        // 새로운 항목을 배열의 처음에 추가
+        return { stockItemList: [newStockItem, ...updatedList] };
+      }
+
+      // 새로운 항목을 배열의 처음에 추가
+      return { stockItemList: [newStockItem, ...state.stockItemList] };
+    }),
   allDeleteStockItem: () => set({ stockItemList: [] }),
 }));
 
