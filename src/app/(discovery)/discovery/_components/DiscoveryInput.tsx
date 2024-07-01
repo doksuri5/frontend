b06d719cb4a7ea2825a369d4e9addd68e8a56ed6
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-01 23:31:08 +0900

    refactor: useState to useRef in DiscoveryInput

--- src/app/(discovery)/discovery/_components/DiscoveryInput.tsx
+++ src/app/(discovery)/discovery/_components/DiscoveryInput.tsx
@@ -1,34 +1,38 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useRef } from "react";
 import { Input } from "@/components/common";
 import SearchIcon from "@/public/icons/search_icon.svg?component";
 
-const DiscoveryInput = ({ param }: { param: string }) => {
+const DiscoveryInput = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const searchParams = useSearchParams();
   const router = useRouter();
-  const [value, setValue] = useState(param || "");
+  const pathname = usePathname();
 
   const moveLink = () => {
-    if (value === "") {
-      router.push(`/discovery`);
-      setValue("");
+    const params = new URLSearchParams(searchParams.toString());
+    if (inputRef.current?.value.trim() === "") {
+      params.delete("search");
     } else {
-      router.push(`/discovery?search=${value}`);
-      setValue(value);
+      params.set("search", inputRef.current?.value || "");
     }
+
+    // window.history.pushState(null, "", `?${params.toString()}`);
+    const newPath = `${pathname}?${params.toString()}`;
+    router.push(newPath);
   };
 
   return (
     <section className="relative h-[5.6rem] w-full">
       <SearchIcon className="absolute left-[1.6rem] top-[1.6rem] z-10 cursor-pointer" onClick={moveLink} />
       <Input
+        ref={inputRef}
         type="text"
-        value={value}
         inputGroupClass="w-full h-[5.6rem]"
         inputClass="text-navy-900 h-[5.6rem] pl-[4.4rem] rounded-[0.8rem]"
         placeholder="종목을 검색해주세요"
-        onChange={(e) => setValue(e.target.value)}
         onKeyUp={(e) => e.key === "Enter" && moveLink()}
       />
     </section>
