4a010820d921879fcded553cde06312ba65c1c9d
yoonyesol <maniac970430@naver.com>
2024-07-01 16:24:22 +0900

    refactor: use-disclosure 훅을 이용한 모달 상태 관리

--- src/app/(mypage)/mypage/page.tsx
+++ src/app/(mypage)/mypage/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "@/components/common";
 import Image from "next/image";
 import Profile from "@/public/icons/profile_icon.svg";
@@ -8,33 +7,27 @@ import ProfileEditModal from "./_components/ProfileEditModal";
 import PWCheckModal from "./_components/PWCheckModal";
 import PrivacyEditModal from "./_components/PrivacyEditModal";
 import WithdrawModal from "./_components/WithdrawModal";
+import useDisclosure from "@/hooks/use-disclosure";
 
 export default function MyPage() {
   //프로필 수정 모달
-  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
-  const openProfileModal = () => setIsProfileModalOpen(true);
-  const closeProfileModal = () => setIsProfileModalOpen(false);
-
+  const profileModal = useDisclosure();
   //패스워드 확인 모달
-  const [isPWCheckModalOpen, setIsPWCheckModalOpen] = useState(false);
-  const openPWCheckModal = () => setIsPWCheckModalOpen(true);
-  const closePWCheckModal = () => {
-    setIsPWCheckModalOpen(false);
-    openPrivacyModal();
-  };
-
+  const pwCheckModal = useDisclosure();
   //개인정보 수정 모달
-  const [isPrivacyModalOpen, setIsPrivacyModalOpen] = useState(false);
-  const openPrivacyModal = () => setIsPrivacyModalOpen(true);
-  const closePrivacyModal = () => setIsPrivacyModalOpen(false);
-
+  const privacyModal = useDisclosure();
   //회원탈퇴 모달
-  const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
-  const openWithdrawModal = () => {
-    closePrivacyModal();
-    setIsWithdrawModalOpen(true);
+  const withdrawModal = useDisclosure();
+
+  const handlePWCheckModalClose = () => {
+    pwCheckModal.close();
+    privacyModal.open();
   };
-  const closeWithdrawModal = () => setIsWithdrawModalOpen(false);
+
+  const handleOpenWithdrawModal = () => {
+    privacyModal.close();
+    withdrawModal.open();
+  };
 
   return (
     <main className="flex flex-col gap-[3.2rem]">
@@ -44,7 +37,7 @@ export default function MyPage() {
           <h1 className="body_2 font-bold text-gray-900">프로필 설정</h1>
           <div>서비스 사용시 보여지는 프로필을 생성 및 변경합니다. 프로필을 설정해보세요.</div>
         </div>
-        <Button variant="textButton" size="sm" className="w-[16rem] text-grayscale-0" onClick={openProfileModal}>
+        <Button variant="textButton" size="sm" className="w-[16rem] text-grayscale-0" onClick={profileModal.open}>
           프로필 수정
         </Button>
       </section>
@@ -55,7 +48,7 @@ export default function MyPage() {
           <div className="body_4 font-medium text-black">김스팩</div>
         </div>
       </section>
-      <ProfileEditModal isOpen={isProfileModalOpen} onClose={closeProfileModal} />
+      <ProfileEditModal isOpen={profileModal.isOpen} onClose={profileModal.close} />
 
       {/* 계정 설정 */}
       <section className="flex flex-row items-center justify-between">
@@ -63,7 +56,7 @@ export default function MyPage() {
           <h1 className="body_2 font-bold text-gray-900">계정 설정</h1>
           <div>서비스 이용시 사용되는 계정을 생성 및 변경합니다. 계정을 연동하여 다양한 서비스를 이용해보세요.</div>
         </div>
-        <Button variant="textButton" size="sm" className="w-[16rem] text-grayscale-0" onClick={openPWCheckModal}>
+        <Button variant="textButton" size="sm" className="w-[16rem] text-grayscale-0" onClick={pwCheckModal.open}>
           계정정보 수정
         </Button>
       </section>
@@ -79,9 +72,13 @@ export default function MyPage() {
         <div className="body_3 w-[14.4rem] font-medium text-grayscale-900">생년월일</div>
         <div className="body_4 font-medium text-grayscale-600">991231</div>
       </div>
-      <PWCheckModal isOpen={isPWCheckModalOpen} onClose={closePWCheckModal} />
-      <PrivacyEditModal isOpen={isPrivacyModalOpen} onClose={closePrivacyModal} openWithdrawModal={openWithdrawModal} />
-      <WithdrawModal isOpen={isWithdrawModalOpen} onClose={closeWithdrawModal} />
+      <PWCheckModal isOpen={pwCheckModal.isOpen} onClose={handlePWCheckModalClose} />
+      <PrivacyEditModal
+        isOpen={privacyModal.isOpen}
+        onClose={privacyModal.close}
+        openWithdrawModal={handleOpenWithdrawModal}
+      />
+      <WithdrawModal isOpen={withdrawModal.isOpen} onClose={withdrawModal.close} />
     </main>
   );
 }
