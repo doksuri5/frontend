c3cc2401dbd5bd4bf543a4412b14e4fe4ff95dbc
magrowing <kmagrowing@gmail.com>
2024-07-01 11:59:32 +0900

    feat : 메인(로그인전) UI 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={pretendard.className}>
-      <body className="h-[calc(100vh-8rem)] bg-background-100">
+      <body className="bg-background-100">
         <Header />
         <main className="relative top-[8rem] m-auto min-h-full max-w-[120rem]">
           <QueryProvider>{children}</QueryProvider>
