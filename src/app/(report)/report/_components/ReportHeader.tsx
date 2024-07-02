909e2aa86b2642da089ea6a2698ba4a582e92ead
ono212 <qkrwjdals302@naver.com>
2024-07-02 10:23:54 +0900

    refactor: ReportHeader컴포넌트의 컴포넌트명 수정, 태그 수정
    - RePortHeader -> ReportHeader
    - div -> section
    -> h3 -> h1

--- src/app/(report)/report/_components/ReportHeader.tsx
+++ src/app/(report)/report/_components/ReportHeader.tsx
@@ -5,23 +5,23 @@ type TStockHeader = {
   data: { icon: string; stockKorName: string; stockEngName: string };
 };
 
-export default function RePortHeader({ data }: TStockHeader) {
+export default function ReportHeader({ data }: TStockHeader) {
   const { icon, stockKorName, stockEngName } = data;
   return (
-    <div className="flex_row justify-between">
+    <section className="flex_row justify-between">
       <div className="flex items-center gap-[1.4rem]">
         <div className="relative h-[6.4rem] w-[6.4rem]">
           <Image src={icon} alt="stock-icon" style={{ width: "100%" }} />
         </div>
         <div className="flex_row gap-[0.8rem] text-navy-900">
-          <h3 className="heading_4 font-bold">{stockKorName}</h3>
-          <h3 className="body_2 font-normal">∙</h3>
-          <h3 className="body_2 font-normal">{stockEngName}</h3>
+          <h1 className="heading_4 font-bold">{stockKorName}</h1>
+          <h1 className="body_2 font-normal">∙</h1>
+          <h1 className="body_2 font-normal">{stockEngName}</h1>
         </div>
       </div>
       <Button variant="textButton" size="md" className="w-[18rem] text-white">
         관심종목 추가
       </Button>
-    </div>
+    </section>
   );
 }
