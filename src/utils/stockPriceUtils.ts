f5487533613ff594e459b3531cb2ceed6e0c6ccb
ono212 <qkrwjdals302@naver.com>
2024-07-09 15:24:10 +0900

    feat: 주가가 중립일 경우 처리 추가

--- src/utils/stockPriceUtils.ts
+++ src/utils/stockPriceUtils.ts
@@ -1,11 +1,15 @@
 // 이전 종가 대비 가격 변화 색상을 반환 (+ : 빨간색, - : 파란색)
 export const getCompareToPreviousClosePriceColor = (data: number) => {
-  return data < 0 ? "text-blue-600" : "text-warning-100";
+  if (data === 0) return "text-grayscale-500";
+  else if (data < 0) return "text-blue-600";
+  else return "text-warning-100";
 };
 
 // 이전 종가 대비 가격 변화 화살표 기호를 반환 (+ : ▲, - : ▼)
 export const getCompareToPreviousClosePriceArrow = (data: number) => {
-  return data < 0 ? "▼" : "▲";
+  if (data === 0) return "";
+  else if (data < 0) return "▼";
+  else return "▲";
 };
 
 // 이전 종가 대비 가격 변화를 +, -가 아닌 화살표 기호를 사용하기 때문에 "-" 부호를 없애는 용도
@@ -15,10 +19,14 @@ export const getCompareToPreviousClosePriceSign = (data: number) => {
 
 // 변동률 색상을 반환 (+ : 빨간색, - : 파란색)
 export const getFluctuationsRatioColor = (data: number) => {
-  return data < 0 ? "text-blue-600" : "text-warning-100";
+  if (data === 0) return "text-grayscale-500";
+  else if (data < 0) return "text-blue-600";
+  else return "text-warning-100";
 };
 
 // 변동률 부호를 반환 (+ 일 때만)
 export const getFluctuationsRatioSign = (data: number) => {
-  return data < 0 ? "" : "+";
+  if (data === 0) return "";
+  else if (data < 0) return "-";
+  else return "+";
 };
