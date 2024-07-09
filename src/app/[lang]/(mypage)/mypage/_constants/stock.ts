b5ad45260e5b16fc2e57aa0bab5f3d7af0690c47
yoonyesol <maniac970430@naver.com>
2024-07-09 15:21:24 +0900

    feat: 유저 프로필 폼에 유저 더미 데이터 세팅

---
+++ src/app/[lang]/(mypage)/mypage/_constants/stock.ts
@@ -0,0 +1,8 @@
+export const stockList = [
+  { value: "tsla", label: "#테슬라" },
+  { value: "appl", label: "#애플" },
+  { value: "amzn", label: "#아마존" },
+  { value: "maft", label: "#MS" },
+  { value: "googl", label: "#구글" },
+  { value: "u", label: "#유니티" },
+];
