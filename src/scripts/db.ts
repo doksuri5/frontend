4ab624e1434bd15d219173ba0408cfe52e19d6f3
knoa0405 <knoa0405@gmail.com>
2024-07-11 01:40:14 +0900

    chore: stock codes DB데이터 조회 후 파일 생성 스크립트 구현

---
+++ src/scripts/db.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+// 환경 변수 확인
+if (!process.env.MONGODB_URL) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URL"');
+}
+
+const uri = process.env.MONGODB_URL;
+
+// 글로벌 환경 변수 설정
+let cached = global as typeof globalThis & {
+  mongoose?: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+};
+
+if (!cached.mongoose) {
+  cached.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  // 캐시된 연결 확인
+  if (cached.mongoose!.conn) {
+    console.log("Already connected to MongoDB");
+    return cached.mongoose!.conn;
+  }
+
+  if (!cached.mongoose!.promise) {
+    cached.mongoose!.promise = mongoose.connect(uri).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.mongoose!.conn = await cached.mongoose!.promise;
+    console.log("MongoDB Connected");
+    return cached.mongoose!.conn;
+  } catch (error) {
+    cached.mongoose!.promise = null;
+    console.error(`MongoDB Error: ${error}`);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  console.log("Disconnecting MongoDB");
+  if (cached.mongoose!.conn) {
+    await mongoose.disconnect();
+    cached.mongoose!.conn = null;
+    cached.mongoose!.promise = null;
+    console.log("MongoDB Disconnected");
+  }
+};
+
+export { connectDB, disconnectDB };
---
+++ src/scripts/generate-stock-codes.ts
@@ -0,0 +1,56 @@
+import { connectDB, disconnectDB } from "./db";
+import StockCode from "@/types/schema/stock-code";
+
+const init = async () => {
+  try {
+    await connectDB();
+    const stockCodes = await StockCode.find({});
+
+    type TReutersCodes = "GOOGL.O" | "AMZN.O" | "AAPL.O" | "MSFT.O" | "TSLA.O" | "U";
+
+    const reutersCodesSet = new Set<string>();
+    const STOCK_CODES: { [key: string]: TReutersCodes } = {};
+    const STOCK_NAMES: { [key in TReutersCodes]: string } = {
+      "GOOGL.O": "",
+      "AMZN.O": "",
+      "AAPL.O": "",
+      "MSFT.O": "",
+      "TSLA.O": "",
+      U: "",
+    };
+
+    stockCodes.forEach((stock) => {
+      const code = stock.code as TReutersCodes;
+      reutersCodesSet.add(code);
+      STOCK_CODES[stock.name.toUpperCase()] = code;
+      STOCK_NAMES[code] = stock.name.toLowerCase();
+    });
+
+    const REUTERS_CODES = Array.from(reutersCodesSet) as TReutersCodes[];
+
+    const content = `
+    // stockName: reutersCode
+    // fetch from db
+    type TReutersCodes = ${REUTERS_CODES.map((code) => `"${code}"`).join(" | ")};
+    
+    const REUTERS_CODES = ${JSON.stringify(REUTERS_CODES, null, 2)} as const;
+    
+    const STOCK_CODES: { [key: string]: TReutersCodes } = ${JSON.stringify(STOCK_CODES, null, 2)};
+    
+    const STOCK_NAMES: { [key in TReutersCodes]: string } = ${JSON.stringify(STOCK_NAMES, null, 2)};
+    
+    export { STOCK_CODES, STOCK_NAMES, REUTERS_CODES };
+    
+    export type { TReutersCodes };
+    `;
+
+    return content;
+  } catch (error) {
+    console.error("Error initializing stockCodes.ts file:", error);
+    process.exit(1);
+  } finally {
+    await disconnectDB();
+  }
+};
+
+export default init;
