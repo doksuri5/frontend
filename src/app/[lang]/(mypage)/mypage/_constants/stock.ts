b8bb14cce2f499fb665131d892c9d5dd000f4812
yoonyesol <maniac970430@naver.com>
2024-07-10 17:48:55 +0900

    feat: 유저 더미데이터 프로필 및 개인정보 수정 기능 구현

--- src/app/[lang]/(mypage)/mypage/_constants/stock.ts
+++ src/app/[lang]/(mypage)/mypage/_constants/stock.ts
@@ -2,7 +2,7 @@ export const stockList = [
   { value: "tsla", label: "#테슬라" },
   { value: "appl", label: "#애플" },
   { value: "amzn", label: "#아마존" },
-  { value: "maft", label: "#MS" },
+  { value: "msft", label: "#MS" },
   { value: "googl", label: "#구글" },
   { value: "u", label: "#유니티" },
 ];
