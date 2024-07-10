d6dad131906cc145ad9b4ddf303f52a4507bef70
knoa0405 <knoa0405@gmail.com>
2024-07-10 14:59:37 +0900

    chore : next config cache debug 세팅, 야후 파이낸스 패키지 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "imgnews.pstatic.net" },
