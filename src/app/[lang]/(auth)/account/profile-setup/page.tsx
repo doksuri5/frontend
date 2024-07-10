54946679086a7ae63b57d95414fed47403e21cbf
magrowing <kmagrowing@gmail.com>
2024-07-10 19:12:34 +0900

    refactor : 소셜로그인시 가입 여부에 따라 페이지 redirect 조건 처리

--- src/app/[lang]/(auth)/account/profile-setup/page.tsx
+++ src/app/[lang]/(auth)/account/profile-setup/page.tsx
@@ -16,19 +16,20 @@ export default function ProfileSetup() {
   const router = useRouter();
 
   useEffect(() => {
-    //if (status === "loading") return;
-
-    if (session) {
-      if (session.user.role === "user") {
-        router.push(HOME_PATH);
-      } else {
-        router.push(PROFILE_SETUP_PATH);
-      }
+    if (!session || session.user.role !== "user") {
+      router.push(PROFILE_SETUP_PATH);
+    } else if (session.user.role === "user") {
+      router.push(HOME_PATH);
     }
   }, [session, status, router]);
 
   if (status === "loading") {
     return <Loading />;
+  }
+
+  // 소설 session 존재, 가입된 유저의 경우 null 처리로 아무것도 렌더링 하지 않도록 처리
+  if (session?.user?.role === "user") {
+    return null;
   }
 
   return (
--- src/app/[lang]/(auth)/account/register/page.tsx
+++ src/app/[lang]/(auth)/account/register/page.tsx
@@ -12,18 +12,13 @@ import { HOME_PATH, REGISTER_PATH } from "@/routes/path";
 
 export default function RegisterPage() {
   const { data: session, status } = useSession();
-
   const router = useRouter();
 
   useEffect(() => {
-    //if (status === "loading") return;
-
-    if (session?.user) {
-      if (session.user.role === "user") {
-        router.push(HOME_PATH);
-      } else {
-        router.push(REGISTER_PATH);
-      }
+    if (!session || session.user.role !== "user") {
+      router.push(REGISTER_PATH);
+    } else if (session.user.role === "user") {
+      router.push(HOME_PATH);
     }
   }, [session, status, router]);
 
@@ -31,11 +26,14 @@ export default function RegisterPage() {
     return <Loading />;
   }
 
+  // 소설 session 존재, 가입된 유저의 경우 null 처리로 아무것도 렌더링 하지 않도록 처리
+  if (session?.user?.role === "user") {
+    return null;
+  }
+
   return (
-    <>
-      <CommonLayout title="회원가입">
-        <RegisterForm />
-      </CommonLayout>
-    </>
+    <CommonLayout title="회원가입">
+      <RegisterForm />
+    </CommonLayout>
   );
 }
