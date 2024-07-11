43e5231a111b7a3a8d7732263057770db0f76683
knoa0405 <knoa0405@gmail.com>
2024-07-11 16:20:00 +0900

    feat: mystock 페이지 api 연결

--- src/app/[lang]/(mystock)/mystock/page.tsx
+++ src/app/[lang]/(mystock)/mystock/page.tsx
@@ -1,69 +1,8 @@
 import { MyStockHeader, MyStockBody, MyStockModalSection } from "@/app/[lang]/(mystock)/mystock/_components";
 import { SearchDetailDataType, StockDataType } from "@/types";
-import Apple_icon from "@/public/icons/Apple_icon.svg";
-
-const stockList: StockDataType[] = [
-  {
-    _id: "1",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: 1.75,
-    fluctuationsRatio: 0.82,
-  },
-  {
-    _id: "2",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: -1.75,
-    fluctuationsRatio: -0.82,
-  },
-  {
-    _id: "3",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: -1.75,
-    fluctuationsRatio: -0.82,
-  },
-  {
-    _id: "4",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: -1.75,
-    fluctuationsRatio: -0.82,
-  },
-  {
-    _id: "5",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: -1.75,
-    fluctuationsRatio: -0.82,
-  },
-  {
-    _id: "6",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: -1.75,
-    fluctuationsRatio: -0.82,
-  },
-];
+import { ApiResponse } from "@/types/ResponseType";
+import { RecentSearchSchema } from "@/types/SearchDataType";
+import { StockSchema } from "@/types/StockDataType";
 
 // db에서 유저 이름 가져오기
 const userName = "김스팩";
@@ -72,29 +11,35 @@ const userName = "김스팩";
 // 최근 검색어 데이터를 db에서 가져오고
 // 네이버 주식 api 데이터를 가져와서
 // symbolCode에 맞게 병합 처리 후 클라이언트로 보내기
-const recentSearchList: SearchDetailDataType[] = [
-  {
-    _id: "1",
-    user_id: "",
-    icon: Apple_icon,
-    stockName: "애플",
-    symbolCode: "AAPL",
-    price: 0.0,
-    nationType: "USA",
-    compareToPreviousClosePrice: 1.75,
-    fluctuationsRatio: 0.82,
-    created_at: "06.14",
-  },
-];
+const getStocks = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/interest/getDetailInterestStocks`, {
+    cache: "no-cache",
+  });
 
-export default function MyStockPage() {
-  const data = stockList;
+  const data = await res.json();
+
+  return ApiResponse(StockSchema).safeParse(data).data;
+};
+
+const getRecentSearchList = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/getRecentSearches`, {
+    cache: "no-cache",
+  });
+
+  const data = await res.json();
+
+  return ApiResponse(RecentSearchSchema).safeParse(data).data;
+};
+
+export default async function MyStockPage() {
+  const stocksData = (await getStocks())?.data;
+  const recentSearchedData = (await getRecentSearchList())?.data;
 
   return (
     <>
       <MyStockHeader userName={userName} />
-      <MyStockBody dataList={data} />
-      <MyStockModalSection dataList={data} recentSearchList={recentSearchList} />
+      <MyStockBody dataList={stocksData ?? []} />
+      <MyStockModalSection dataList={stocksData ?? []} recentSearchList={recentSearchedData ?? []} />
     </>
   );
 }
