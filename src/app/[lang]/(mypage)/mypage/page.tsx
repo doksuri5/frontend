e7be673e780f827cae65cf3b200ab377cf7efdb4
yoonyesol <maniac970430@naver.com>
2024-07-13 01:18:34 +0900

    feat: 실제 유저 데이터 가져오기 및 실제 유저 비밀번호 인증 구현

--- src/app/[lang]/(mypage)/mypage/page.tsx
+++ src/app/[lang]/(mypage)/mypage/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import MyPageMain from "./_components/MyPageMain";
-
-// TODO: 추후 실제 유저 이메일 가져오도록 로직 수정 예정
-const email = "abcde@test.com";
+import { auth } from "@/auth"  
 
 const fetchUserData = async () => {
+  const session = await auth();
   try {
     const response = await (
-      await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getUser/${email}`, { cache: "no-cache" })
+      await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getUser/${session?.user.email}`, { cache: "no-cache" })
     ).json();
 
     if (response.ok) {
