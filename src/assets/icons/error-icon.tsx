a0843cb4a48d325eb2ef37d874e8263d8766c42a
magrowing <kmagrowing@gmail.com>
2024-07-12 21:14:32 +0900

    refactor : Icon 컴포넌트 error 수정

--- src/assets/icons/error-icon.tsx
+++ src/assets/icons/error-icon.tsx
@@ -1,9 +1,9 @@
 const ErrorIcon = ({ color = "#FF294F" }: { color?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={color} width={24} height={24}>
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-      clip-rule="evenodd"
+      clipRule="evenodd"
     />
   </svg>
 );
