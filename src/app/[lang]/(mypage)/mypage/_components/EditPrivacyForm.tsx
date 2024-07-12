fb8e4433b9f48487f7c188184517a3bf98dbd684
yoonyesol <maniac970430@naver.com>
2024-07-12 15:50:20 +0900

    feat: 다양한 사용자 입력 상황에 따른 버튼 disable 처리 및 validation 패턴 수정

--- src/app/[lang]/(mypage)/mypage/_components/EditPrivacyForm.tsx
+++ src/app/[lang]/(mypage)/mypage/_components/EditPrivacyForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Button, Input } from "@/components/common";
 import { cn } from "@/utils/cn";
@@ -21,13 +21,10 @@ interface FormData {
 export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
   const { userStoreData } = useUserStore();
   const router = useRouter();
-  const [formValid, setFormValid] = useState(false);
   const {
     register,
     handleSubmit,
     watch,
-    setError,
-    clearErrors,
     formState: { errors, isDirty, isValid },
   } = useForm<FormData>({
     mode: "onChange",
@@ -40,14 +37,13 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
     },
   });
 
-  const watchRegister = watch();
-  //실제 값이 입력된 필드만을 고려하여 폼의 유효성을 검사
-  const hasValues = Object.values(watchRegister).every((value) => value !== undefined && value !== "");
-  const isFormValid = !Object.keys(errors).length && hasValues && formValid;
+  const password = watch("password");
+  const passwordChk = watch("passwordChk");
+  const formValid = Boolean(isValid && isDirty && (!password || password === passwordChk));
 
-  //Form 전송 함수
+  // Form 전송 함수
   const onSubmit = async (data: FormData) => {
-    if (!isValid || !isDirty) return;
+    if (!formValid) return;
     console.log(data);
 
     const formData = {
@@ -78,8 +74,6 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
     }
   };
 
-  const password = watch("password");
-
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={cn("flex w-full flex-col gap-[1.6rem]")}>
       <Input id="email" labelName="이메일" value={userStoreData?.email} disabled />
@@ -90,7 +84,6 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
           labelName="새 비밀번호 입력"
           placeholder="변경할 비밀번호를 입력해주세요."
           {...register("password", {
-            required: "비밀번호를 입력해주세요.",
             minLength: {
               value: 8,
               message: "비밀번호는 최소 8자리 이상이어야 합니다.",
@@ -118,7 +111,6 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
           labelName="새 비밀번호 확인"
           placeholder="비밀번호를 다시 한번 입력해주세요."
           {...register("passwordChk", {
-            required: "비밀번호 확인을 입력해주세요.",
             validate: (value) => value === password || "비밀번호가 일치하지 않습니다.",
           })}
           variant={errors.passwordChk ? "error" : "default"}
@@ -133,9 +125,9 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
           placeholder="-를 제외한 휴대폰번호를 입력해주세요."
           {...register("phone", {
             required: "휴대폰 번호를 입력해주세요.",
-            pattern: {
-              value: /^\d{10,11}$/,
-              message: "유효한 휴대폰 번호를 입력해주세요.",
+            validate: {
+              containsHyphen: (value) => !/-/.test(value) || "-를 제외한 휴대폰번호를 입력해주세요.",
+              correctLength: (value) => /^\d{10,11}$/.test(value) || "유효한 휴대폰 번호를 입력해주세요.",
             },
           })}
           variant={errors.phone ? "error" : "default"}
@@ -150,9 +142,10 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
           placeholder="생년월일 6자리를 입력해주세요.(예시 : 991231)"
           {...register("birth", {
             required: "생년월일을 입력해주세요.",
-            pattern: {
-              value: /^\d{6}$/,
-              message: "생년월일은 6자리 숫자여야 합니다.",
+            validate: {
+              correctLength: (value) => value.length === 6 || "생년월일은 반드시 6자리 숫자여야 합니다.",
+              validFormat: (value) =>
+                /^[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/.test(value) || "생년월일 형식이 올바르지 않습니다.",
             },
           })}
           variant={errors.birth ? "error" : "default"}
@@ -163,9 +156,9 @@ export default function EditPrivacyForm({ closeModal }: TEditPrivacyFormProps) {
       <Button
         type="submit"
         size="lg"
-        bgColor={isValid && isDirty ? "bg-navy-900" : "bg-grayscale-200"}
-        className={cn(`mt-[4rem] ${isValid && isDirty ? "text-white" : "text-gray-300"}`)}
-        disabled={!isValid || !isDirty}
+        bgColor={formValid ? "bg-navy-900" : "bg-grayscale-200"}
+        className={cn(`mt-[4rem] ${formValid ? "text-white" : "text-gray-300"}`)}
+        disabled={!formValid}
       >
         수정하기
       </Button>
