9fb2e7fd2235a8e0bd9a39c8b881006d6544879e
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-16 02:16:28 +0900

    feat: 주식 검색 뉴스 API 적용

--- src/app/[lang]/(discovery)/discovery/_components/DiscoveryNews.tsx
+++ src/app/[lang]/(discovery)/discovery/_components/DiscoveryNews.tsx
@@ -1,72 +1,38 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { FindNews, FindNewsSkeleton } from "@/components/common";
-import NewsImage from "@/public/icons/news.jpg";
 import DiscoverySection from "./DiscoverySection";
+import { getSearchNews } from "@/actions/news";
+import { useEffect, useState } from "react";
+import { SearchStockNewsDataType } from "@/types/NewsDataType2";
 
-const News = ({ param }: { param: string }) => {
-  // 로딩 이벤트를 줄라고 useState, useEffect를 사용하기 때문에 클라이언트 컴포넌트로 선언했습니다.
-  // 추후에 없앨겁니다.
-  const [newsList, setNewsList] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
+const News = ({ params }: { params: string }) => {
+  const [newsList, setNewsList] = useState<SearchStockNewsDataType[]>([]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setNewsList([
-        {
-          idx: 1,
-          image: NewsImage,
-          title: "\"산유국 되나\" 尹 한 마디에 한국석유 또 '上'…석유주 훨훨",
-          publishedTime: "n",
-          newspaperCompany: "문화일보",
-        },
-        {
-          idx: 2,
-          image: NewsImage,
-          title: "\"산유국 되나\" 尹 한 마디에 한국석유 또 '上'…석유주 훨훨",
-          publishedTime: "n",
-          newspaperCompany: "문화일보",
-        },
-        {
-          idx: 3,
-          image: NewsImage,
-          title: "\"산유국 되나\" 尹 한 마디에 한국석유 또 '上'…석유주 훨훨",
-          publishedTime: "n",
-          newspaperCompany: "문화일보",
-        },
-        {
-          idx: 4,
-          image: NewsImage,
-          title: "\"산유국 되나\" 尹 한 마디에 한국석유 또 '上'…석유주 훨훨",
-          publishedTime: "n",
-          newspaperCompany: "문화일보",
-        },
-        {
-          idx: 5,
-          image: NewsImage,
-          title: "\"산유국 되나\" 尹 한 마디에 한국석유 또 '上'…석유주 훨훨",
-          publishedTime: "n",
-          newspaperCompany: "문화일보",
-        },
-        {
-          idx: 6,
-          image: NewsImage,
-          title: "\"산유국 되나\" 尹 한 마디에 한국석유 또 '上'…석유주 훨훨",
-          publishedTime: "n",
-          newspaperCompany: "문화일보",
-        },
-      ]);
-      setLoading(false);
-    }, 2000);
-  }, []);
+    const fetchNews = async () => {
+      try {
+        const response = await getSearchNews(params);
+        if (response.ok) {
+          setNewsList(response.data);
+        } else {
+          console.log("Error: Response is not an array");
+        }
+      } catch (error) {
+        console.error("Fetch Error:", error);
+      }
+    };
 
-  const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`(${newsList.length})`}</span>;
+    fetchNews();
+  }, [params]);
+
+  // const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`(${newsList.length})`}</span>;
+  const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`()`}</span>;
 
   return (
     <DiscoverySection title="뉴스" subTag={subTag}>
       <div className="flex_col h-vh gap-[1.6rem] rounded-[1.6rem] bg-white p-[2.4rem]">
-        {loading ? (
+        {/* {loading ? (
           <>
             {Array.from({ length: 6 }).map((_, idx) => (
               <FindNewsSkeleton key={idx} />
@@ -86,7 +52,7 @@ const News = ({ param }: { param: string }) => {
             <hr className="mb-[1.6rem] mt-[1.8rem]" />
             <p className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400">더보기</p>
           </>
-        )}
+        )} */}
       </div>
     </DiscoverySection>
   );
