bf952fb45bb870ae358b9e3b2b485c18407a76c8
knoa0405 <knoa0405@gmail.com>
2024-07-16 14:39:25 +0900

    feat:  stock analysis api 기능 일부 수정

--- src/app/api/report/stock-analysis/[code]/route.ts
+++ src/app/api/report/stock-analysis/[code]/route.ts
@@ -1,33 +1,53 @@
 import type { NextRequest } from "next/server";
 import { createOpenAI } from "@ai-sdk/openai";
 import { generateObject } from "ai";
-import yahooFinance from "yahoo-finance2";
 import { z } from "zod";
+import { FetchedNaverStockDataType } from "@/types/StockDataType";
+import { kv } from "@vercel/kv";
+import yahooFinance from "yahoo-finance2";
 
 export const maxDuration = 30;
 
-export const GET = async (req: NextRequest, { params }: { params: { code: string } }) => {
+export const GET = async (request: NextRequest, { params }: { params: { code: string } }) => {
   try {
-    const quoteSummary = await yahooFinance.quoteSummary(params.code, {
+    const key = `stock:${params.code}`;
+    const cached = await kv.hgetall(key);
+
+    if (cached) {
+      return Response.json({
+        ok: true,
+        data: [
+          {
+            ...cached,
+          },
+        ],
+      });
+    }
+
+    const stockInfo = await fetch(`https://api.stock.naver.com/stock/${params.code}/basic`);
+    const stockInfoData = (await stockInfo.json()) as FetchedNaverStockDataType;
+
+    const quoteSummary = await yahooFinance.quoteSummary(stockInfoData.symbolCode, {
       modules: ["financialData", "summaryDetail", "defaultKeyStatistics", "price"],
     });
 
     const metrics = {
-      companyName: quoteSummary.price?.longName,
+      stockName: stockInfoData.stockName,
+      stockNameEng: stockInfoData.stockNameEng,
       ROE: quoteSummary.financialData?.returnOnEquity,
       PER: quoteSummary.summaryDetail?.trailingPE,
       forwardPER: quoteSummary.summaryDetail?.forwardPE,
       dividendYield: quoteSummary.summaryDetail?.dividendYield,
+      currentPrice: stockInfoData.closePrice,
       currentPriceRatio: quoteSummary.financialData?.currentRatio,
-      currentPrice: quoteSummary.price?.regularMarketPrice,
+      currentPriceChange: quoteSummary.price?.regularMarketChange,
     };
 
-    const llamaAI = createOpenAI({
-      apiKey: process.env.TOGETHER_API_KEY,
-      baseURL: process.env.TOGETHER_API_URL,
+    const ai = createOpenAI({
+      apiKey: process.env.OPENAI_KEY,
     });
 
-    const model = llamaAI.chat("togethercomputer/CodeLlama-34b-Instruct");
+    const model = ai.chat("gpt-4o");
 
     const response = await generateObject({
       model,
@@ -40,11 +60,10 @@ export const GET = async (req: NextRequest, { params }: { params: { code: string
       }),
       system:
         "You are a stock analyst. You are analyzing a stock and providing a report on its profitability, interest, and growth potential.",
-      temperature: 0.7,
       prompt: `
         s&p500 is a trailing PER of 20.36, which is the average for the past 10 years.
         The S&P500 average is 22.93 Forward PER (the next 12 months).
-        The company is ${metrics.companyName}.
+        The company is ${metrics.stockNameEng}.
         The company's ROE is ${metrics.ROE}.
         The company's PER is ${metrics.PER}.
         The company's forward PER is ${metrics.forwardPER}.
@@ -53,13 +72,7 @@ export const GET = async (req: NextRequest, { params }: { params: { code: string
         The company's current price ratio is ${metrics.currentPriceRatio}.
         if per is higher than 20.36, the company is considered overvalued. and forward per is higher than 22.93, the company is considered overvalued.
         if company forward per is lower then trailing per, It can be expected to grow gradually.
-  
         please provide a report on the profitability, interest, and growth potential of the company and the reason for your analysis.
-        example of analysisReason : "We raise our price target on Tesla by 26% to $340 and maintain our Top Pick, reflecting robust auto demand despite sharp rate hikes. 
-        While we expect a short-term pause in the upside, the company's competitive position is stronger in the medium term. With traditional OEMs 
-        slow to transition to EVs and Chinese start-ups' cash flows weakening, Tesla's Mexican factory is expected to widen the EV manufacturing competitiveness gap."
-        Don't use this example. it is just an example. please provide your own analysis report. Be a little more specific about why.
-  
         profitabilityPercentage: An indicator of how much you can expect to earn in a year's time, expressed as a percentage.
         interestPercentage: An indicator of how much people are interested in this stock recently use search , expressed as a percentage.
         growthPercentage: An indicator of how much the company is expected to grow in the future, expressed as a percentage.
@@ -67,11 +80,32 @@ export const GET = async (req: NextRequest, { params }: { params: { code: string
       `,
     });
 
-    return Response.json({
+    const returnMetrics = {
+      stockName: metrics.stockName,
+      symbolCode: params.code,
+      fluctuationsRatio: metrics.currentPriceRatio,
+      closePriceChange: metrics.currentPriceChange,
+      closePrice: metrics.currentPrice,
+    };
+
+    await kv.hmset(key, {
       ...response.object,
-      metrics,
+      metrics: returnMetrics,
     });
-  } catch {
+
+    await kv.expire(key, 60 * 60 * 6);
+
+    return Response.json({
+      ok: true,
+      data: [
+        {
+          ...response.object,
+          metrics: returnMetrics,
+        },
+      ],
+    });
+  } catch (error) {
+    console.error(error);
     return Response.error();
   }
 };
