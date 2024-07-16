5e731316043d82076a397a67a7f449a37723b25a
윤태현 <yoonth0919@gmail.com>
2024-07-16 22:33:38 +0900

    feat: 발견 페이지 뉴스 컴포넌트 API 적용

--- src/app/[lang]/(discovery)/discovery/_components/DiscoveryNews.tsx
+++ src/app/[lang]/(discovery)/discovery/_components/DiscoveryNews.tsx
@@ -1,18 +1,23 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { FindNews, FindNewsSkeleton } from "@/components/common";
 import DiscoverySection from "./DiscoverySection";
 import { getSearchNews } from "@/actions/news";
-import { useEffect, useState } from "react";
 import { SearchStockNewsDataType } from "@/types/NewsDataType2";
 
 const News = ({ params }: { params: string }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [newsList, setNewsList] = useState<SearchStockNewsDataType[]>([]);
+  const pathname = usePathname();
+  const lang = pathname.match(/\/([a-z]{2})\//)?.[1];
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const response = await getSearchNews(params);
+        setIsLoading(true);
+        const response = await getSearchNews(undefined, { params: params });
         if (response.ok) {
           setNewsList(response.data);
         } else {
@@ -20,19 +25,20 @@ const News = ({ params }: { params: string }) => {
         }
       } catch (error) {
         console.error("Fetch Error:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchNews();
   }, [params]);
 
-  // const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`(${newsList.length})`}</span>;
-  const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`()`}</span>;
+  const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`(${newsList.length})`}</span>;
 
   return (
     <DiscoverySection title="뉴스" subTag={subTag}>
       <div className="flex_col h-vh gap-[1.6rem] rounded-[1.6rem] bg-white p-[2.4rem]">
-        {/* {loading ? (
+        {isLoading ? (
           <>
             {Array.from({ length: 6 }).map((_, idx) => (
               <FindNewsSkeleton key={idx} />
@@ -40,19 +46,30 @@ const News = ({ params }: { params: string }) => {
           </>
         ) : (
           <>
-            {newsList.map((news) => (
-              <FindNews
-                key={news.idx}
-                image={news.image}
-                title={news.title}
-                publishedTime={news.publishedTime}
-                newspaperCompany={news.newspaperCompany}
-              />
-            ))}
+            {newsList.map((news) => {
+              const title =
+                lang && news.title[lang as keyof typeof news.title]
+                  ? news.title[lang as keyof typeof news.title]
+                  : news.title["ko"];
+              const publisher =
+                lang && news.publisher[lang as keyof typeof news.publisher]
+                  ? news.publisher[lang as keyof typeof news.publisher]
+                  : news.publisher["ko"];
+              return (
+                <FindNews
+                  key={news.index}
+                  _id={news.index}
+                  image={news.thumbnailUrl}
+                  title={title}
+                  publishedTime={news.publishedTime}
+                  newspaperCompany={publisher}
+                />
+              );
+            })}
             <hr className="mb-[1.6rem] mt-[1.8rem]" />
             <p className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400">더보기</p>
           </>
-        )} */}
+        )}
       </div>
     </DiscoverySection>
   );
