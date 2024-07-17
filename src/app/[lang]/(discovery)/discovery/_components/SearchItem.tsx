142a4c389ae2971123576bc6a851b4600fc24bc3
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-18 03:28:31 +0900

    refactor: 검색 컴포넌트 생성

---
+++ src/app/[lang]/(discovery)/discovery/_components/SearchItem.tsx
@@ -0,0 +1,22 @@
+import TimeIcon from "@/public/icons/time_icon.svg?component";
+import CloseIcon from "@/public/icons/close_icon.svg?component";
+import { SearchRecentDataType } from "@/types/RecentDataType";
+
+const SearchItem = ({ search, deleteSearch }: { search: SearchRecentDataType; deleteSearch: () => void }) => {
+  return (
+    <li key={search.searchText} className="flex_row h-[4rem] w-full justify-between">
+      <div className="flex_row gap-[.8rem]">
+        <TimeIcon width={24} height={24} fill="text-navy-900" />
+        <span className="body_4 font-medium text-grayscale-600">{search.searchText}</span>
+      </div>
+      <div className="flex_row gap-[.8rem]">
+        <span className="body_5 text-grayscale-400">{search.searchDate.split("T")[0].slice(5)}</span>
+        <button type="button" onClick={deleteSearch}>
+          <CloseIcon />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default SearchItem;
