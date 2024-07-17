bdb38a62bea1fc2cce097966f9ec96936ddd9a2b
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-18 03:30:44 +0900

    refactor: loading to render 변경 및 최적화

--- src/app/[lang]/(discovery)/discovery/_components/DiscoveryNews.tsx
+++ src/app/[lang]/(discovery)/discovery/_components/DiscoveryNews.tsx
@@ -1,44 +1,54 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { FindNews, FindNewsSkeleton } from "@/components/common";
 import DiscoverySection from "./DiscoverySection";
 import { getSearchNews } from "@/actions/news";
 import { SearchStockNewsDataType } from "@/types/NewsDataType2";
 
 const News = ({ params }: { params: string }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [newsList, setNewsList] = useState<SearchStockNewsDataType[]>([]);
   const pathname = usePathname();
   const lang = pathname.match(/\/([a-z]{2})\//)?.[1];
+  const [isRander, setIsRender] = useState(false);
+  const [newsList, setNewsList] = useState<SearchStockNewsDataType[]>([]);
+  const [showMoreItems, setShowMoreItems] = useState(false);
+  const [maxDisplayedItems, setMaxDisplayedItems] = useState(6);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        setIsLoading(true);
         const response = await getSearchNews(undefined, { params: params });
         if (response.ok) {
           setNewsList(response.data);
-        } else {
-          console.log("Error: Response is not an array");
         }
       } catch (error) {
         console.error("Fetch Error:", error);
       } finally {
-        setIsLoading(false);
+        setIsRender(true);
       }
     };
 
     fetchNews();
   }, [params]);
 
+  const handleShowMore = () => {
+    const nextItemLength = maxDisplayedItems + 6;
+    setMaxDisplayedItems(nextItemLength);
+
+    if (nextItemLength >= newsList.length) setShowMoreItems(false);
+  };
+
+  const displayedStockNews = useMemo(() => {
+    return showMoreItems ? newsList : newsList.slice(0, maxDisplayedItems);
+  }, [newsList, showMoreItems, maxDisplayedItems]);
+
   const subTag = <span className={`body_5 font-medium text-grayscale-600`}>{`(${newsList.length})`}</span>;
 
   return (
     <DiscoverySection title="뉴스" subTag={subTag}>
       <div className="flex_col h-vh gap-[1.6rem] rounded-[1.6rem] bg-white p-[2.4rem]">
-        {isLoading ? (
+        {!isRander ? (
           <>
             {Array.from({ length: 6 }).map((_, idx) => (
               <FindNewsSkeleton key={idx} />
@@ -46,7 +56,7 @@ const News = ({ params }: { params: string }) => {
           </>
         ) : (
           <>
-            {newsList.map((news) => {
+            {displayedStockNews.map((news) => {
               const title =
                 lang && news.title[lang as keyof typeof news.title]
                   ? news.title[lang as keyof typeof news.title]
@@ -66,8 +76,17 @@ const News = ({ params }: { params: string }) => {
                 />
               );
             })}
-            <hr className="mb-[1.6rem] mt-[1.8rem]" />
-            <p className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400">더보기</p>
+            {newsList.length > maxDisplayedItems && !showMoreItems && (
+              <>
+                <hr className="mb-[1.6rem] mt-[1.8rem]" />
+                <p
+                  className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400"
+                  onClick={handleShowMore}
+                >
+                  더보기
+                </p>
+              </>
+            )}
           </>
         )}
       </div>
--- src/app/[lang]/(discovery)/discovery/_components/DiscoveryStocks.tsx
+++ src/app/[lang]/(discovery)/discovery/_components/DiscoveryStocks.tsx
@@ -8,20 +8,19 @@ import { saveRecentSearch } from "@/actions/stock";
 
 const Stocks = ({ params }: { params: string }) => {
   const [stockList, setStockList] = useState<StockDataType[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [showMoreItems, setShowMoreItems] = useState<boolean>(false);
-  const [maxDisplayedItems, setMaxDisplayedItems] = useState<number>(4);
+  const [isRander, setIsRender] = useState(false);
+  const [showMoreItems, setShowMoreItems] = useState(false);
+  const [maxDisplayedItems, setMaxDisplayedItems] = useState(4);
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
       try {
         const response = await saveRecentSearch({ stockName: params });
         if (response.ok) setStockList(response.data);
       } catch (error) {
         console.error("Failed to fetch stock data:", error);
       } finally {
-        setLoading(false);
+        setIsRender(true);
       }
     };
 
@@ -40,21 +39,19 @@ const Stocks = ({ params }: { params: string }) => {
   return (
     <DiscoverySection title="주식" subTag={subTag}>
       <div className="flex_col rounded-[1.6rem] bg-white p-[2.4rem]">
-        <div className="grid w-full grid-cols-2 gap-x-[1.6rem] gap-y-[.8rem]">
-          {displayedStockList.length > 0 ? (
-            displayedStockList.map((stock: StockDataType) => (
-              <StockItem key={stock.stockName} variant="findStock" {...stock} />
-            ))
-          ) : (
-            <>
-              {Array.from({ length: 4 }).map((_, idx) => (
-                <StockItemSkeleton key={idx} variant="findStock" />
-              ))}
-            </>
-          )}
-        </div>
-        {loading || (
+        {!isRander ? (
+          <div className="grid w-full grid-cols-2 gap-x-[1.6rem] gap-y-[.8rem]">
+            {Array.from({ length: 4 }).map((_, idx) => (
+              <StockItemSkeleton key={idx} variant="findStock" />
+            ))}
+          </div>
+        ) : (
           <>
+            <div className="grid w-full grid-cols-2 gap-x-[1.6rem] gap-y-[.8rem]">
+              {displayedStockList.map((stock: StockDataType) => (
+                <StockItem key={stock.stockName} variant="findStock" {...stock} />
+              ))}
+            </div>
             {stockList.length > maxDisplayedItems && (
               <>
                 <hr className="mb-[1.6rem] mt-[1.8rem]" />
