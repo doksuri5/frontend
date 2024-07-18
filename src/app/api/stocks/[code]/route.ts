b839e8fd52247a98b11d4dfdce4bb77e14890406
knoa0405 <knoa0405@gmail.com>
2024-07-18 14:33:16 +0900

    feat: 주식 종목 api 추가

---
+++ src/app/api/stocks/[code]/route.ts
@@ -0,0 +1,5 @@
+import { NextRequest } from "next/server";
+
+const GET = async (request: NextRequest) => {
+  // const response = await fetch(`https://api.stockanalysis.com/stocks/${request.params.code}`);
+};
