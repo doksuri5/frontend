93624368529f541c523cf388e0440914509c4dcd
ono212 <qkrwjdals302@naver.com>
2024-07-19 13:20:48 +0900

    feat: 관심 종목과 관련된 뉴스 api 추가

--- src/actions/news.ts
+++ src/actions/news.ts
@@ -46,3 +46,38 @@ export const fetchPopularNews = async () => {
     console.error(err);
   }
 };
+
+export const fetchInterestStockNews = async () => {
+  const cookieStore = cookies();
+  const connectCookie = cookieStore.get("connect.sid")?.value;
+
+  try {
+    const response = await (
+      await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/news/getInterestStockNews`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `connect.sid=${connectCookie}`,
+        },
+        credentials: "include",
+      })
+    ).json();
+
+    if (response.ok) {
+      let data = response.data;
+
+      let refinedData = (data as any[]).map(({ content_img, title, published_time, publisher, index }) => ({
+        image: content_img,
+        // TODO: 시간 포맷팅하기
+        date: published_time,
+        _id: index,
+        title: title.ko,
+        publisher: publisher.ko,
+      }));
+
+      return [refinedData[0], refinedData[1], refinedData[2]];
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
