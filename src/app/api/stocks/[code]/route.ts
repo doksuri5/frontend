77519fc3df314b4197666bd384b62e526b480942
knoa0405 <knoa0405@gmail.com>
2024-07-19 13:12:58 +0900

    chore: routes api path 변경

--- src/app/api/stocks/[code]/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextRequest } from "next/server";
-
-const GET = async (request: NextRequest) => {
-  // const response = await fetch(`https://api.stockanalysis.com/stocks/${request.params.code}`);
-};
