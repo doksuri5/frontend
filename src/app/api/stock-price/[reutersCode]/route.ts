946f5d7145035a8b025278809d7d89937546ae85
knoa0405 <knoa0405@gmail.com>
2024-07-21 04:02:22 +0900

    feat: 주식 가격 sse 적용

---
+++ src/app/api/stock-price/[reutersCode]/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from "next/server";
+import yahooFinance from "yahoo-finance2";
+
+export const dynamic = "force-dynamic";
+
+export const GET = async (request: NextRequest, { params }: { params: { reutersCode: string } }) => {
+  const { reutersCode } = params;
+  try {
+    if (!reutersCode) {
+      throw new Error("path parameter is required in stock/[reutersCode = undefined]");
+    }
+    const symbolCode = reutersCode.replace(".O", "");
+
+    const quoteSummary = await yahooFinance.quoteSummary(symbolCode, {
+      modules: ["price"],
+    });
+
+    const data = {
+      symbolCode: quoteSummary.price?.symbol,
+      closePrice: quoteSummary.price?.regularMarketPrice,
+      fluctuationsRatio: quoteSummary.price?.regularMarketChangePercent,
+      compareToPreviousClosePrice: quoteSummary.price?.regularMarketChange,
+    };
+
+    return Response.json({ ok: true, data });
+  } catch (error) {
+    return Response.json({ ok: false, message: "fetch failed from api/stock/[reutersCode]" });
+  }
+};
