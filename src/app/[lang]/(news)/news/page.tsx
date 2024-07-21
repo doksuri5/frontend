20bc50c711b71ae62ea72b60925fa571ebdaf97f
knoa0405 <knoa0405@gmail.com>
2024-07-22 01:22:23 +0900

    fix: error in dictionaries

--- src/app/[lang]/(news)/news/page.tsx
+++ src/app/[lang]/(news)/news/page.tsx
@@ -1,9 +1,5 @@
 import News from "@/app/[lang]/(news)/news/_components";
-import { getDictionary } from "@/lib/get-dictionary";
-import { Locale } from "@/lib/i18n-config";
 
-export default async function NewsPage({ params: { lang } }: { params: { lang: Locale } }) {
-  const dictionary = await getDictionary(lang);
-
+export default async function NewsPage() {
   return <News />;
 }
