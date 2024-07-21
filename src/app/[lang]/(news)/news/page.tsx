e289d4895e477e3d7749fe5af7f1079bb1d8e1e5
knoa0405 <knoa0405@gmail.com>
2024-07-22 01:16:21 +0900

    fix: build error

--- src/app/[lang]/(news)/news/page.tsx
+++ src/app/[lang]/(news)/news/page.tsx
@@ -1,6 +1,6 @@
 import News from "@/app/[lang]/(news)/news/_components";
-import { getDictionary } from "@/get-dictionary";
-import { Locale } from "@/i18n-config";
+import { getDictionary } from "@/lib/get-dictionary";
+import { Locale } from "@/lib/i18n-config";
 
 export default async function NewsPage({ params: { lang } }: { params: { lang: Locale } }) {
   const dictionary = await getDictionary(lang);
