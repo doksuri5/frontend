335f863ec61b1427adad9569162c3ffbc90010d7
knoa0405 <knoa0405@gmail.com>
2024-07-22 17:04:03 +0900

    hotfix: error in news

--- src/app/[lang]/(news)/news/page.tsx
+++ src/app/[lang]/(news)/news/page.tsx
@@ -1,9 +1,11 @@
 import { fetchInterestStockNews, fetchPopularNews, fetchRecentNews } from "@/actions/news";
 import News from "@/app/[lang]/(news)/news/_components";
+import { Locale } from "@/lib/i18n-config";
 
 export default async function NewsPage({ params: { lang } }: { params: { lang: Locale } }) {
-  const dictionary = await getDictionary(lang);
+  // const dictionary = await getDictionary(lang);
 
   const [newsData, interestStockNews] = await Promise.allSettled([fetchPopularNews(), fetchInterestStockNews()]);
 
   return <News popularNews={newsData} interestStockNews={interestStockNews} />;
+}
