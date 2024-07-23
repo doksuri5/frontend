cf380061efdc6b7963ea778226b517ce90fcac62
knoa0405 <knoa0405@gmail.com>
2024-07-23 15:37:07 +0900

    feat: 홈페이지 최근 조회 기능 추가

--- src/app/[lang]/(home)/home/page.tsx
+++ src/app/[lang]/(home)/home/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import News from "./news";
 import AiReports from "./_components/ai-reports";
-import { getDetailInterestStocks } from "@/actions/stock";
+import { getDetailInterestStocks, getRecentSearchDetails } from "@/actions/stock";
 import { StockItem } from "@/components/common";
 import { cn } from "@/utils/cn";
 import { fetchInterestStockNews, fetchPopularNews } from "@/actions/news";
+import { getRecentSearches } from "@/actions/search";
 
 export const dynamic = "force-dynamic";
 
@@ -16,6 +17,8 @@ const getInterestStocks = async () => {
 
 export default async function HomePage() {
   const data = await getInterestStocks();
+  const recentSearches = (await getRecentSearchDetails()).data;
+
   const [newsData, interestStockNews] = await Promise.allSettled([fetchPopularNews(), fetchInterestStockNews()]);
 
   return (
@@ -24,9 +27,22 @@ export default async function HomePage() {
       <section className="flex w-full gap-[2rem]">
         <div className="flex h-[40rem] flex-1 flex-col gap-[2.4rem]">
           <h2 className="heading_4 font-bold">최근 조회</h2>
-          <div className="flex_row_col flex-1 gap-[1.3rem] rounded-[1.6rem] bg-white">
-            <Image src="/icons/warning_icon.svg" alt="waring icon" width={50} height={50} />
-            <p>최근 조회한 종목이 없습니다.</p>
+          <div
+            className={cn(
+              "flex-1 overflow-auto rounded-[1.6rem] bg-white py-[1.2rem] pl-[2.4rem] pr-[3.2rem] scrollbar-hide",
+              {
+                "flex items-center justify-center": !recentSearches || recentSearches.length === 0,
+              },
+            )}
+          >
+            {!recentSearches || recentSearches.length === 0 ? (
+              <div className="flex_row_col flex-1 gap-[1.3rem] rounded-[1.6rem] bg-white">
+                <Image src="/icons/warning_icon.svg" alt="waring icon" width={50} height={50} />
+                <p>최근 조회한 종목이 없습니다.</p>
+              </div>
+            ) : (
+              recentSearches.map((stock) => <StockItem variant="stock" key={stock.id} {...stock} />)
+            )}
           </div>
         </div>
         <div className="flex h-[40rem] flex-1 flex-col gap-[2.4rem]">
