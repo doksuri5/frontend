db685af98cfe836f47df76aa577905696bcffabf
knoa0405 <knoa0405@gmail.com>
2024-07-27 00:29:26 +0900

    feat: 챗봇 다국어 기능 & 주식 설명 api 추가

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -7,41 +7,51 @@ import { z } from "zod";
 import yahooFinance from "yahoo-finance2";
 
 export const dynamic = "force-dynamic";
-
 export const maxDuration = 30;
 
 export const POST = async (req: NextRequest) => {
   const { messages } = await req.json();
   const session = await auth();
+  try {
+    const ai = createOpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
 
-  const ai = createOpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+    const model = ai.chat(AI_MODEL);
+    const result = await streamText({
+      model,
+      system: `You are a helpful stock analyst. Answer my questions about the stock market. please answer in ${LANGUAGE_MAP[session?.user.language ?? "ko"]}.`,
+      tools: {
+        getStockInformation: tool({
+          description: "Get the stock information from yahoo finance",
+          parameters: z.object({
+            stockSymbolCode: z.string().describe("It's a symbol code of the stock example: AAPL"),
+            reutersCode: z.string().describe("It's a reuters code of the stock example: AAPL.O"),
+          }),
+          execute: async ({ stockSymbolCode, reutersCode }) => {
+            const quoteSummary = await yahooFinance.quoteSummary(stockSymbolCode);
+            const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/stock/${reutersCode}/description`).then(
+              (res) => res.json(),
+            );
+            const description = data.data.corporateOverview;
 
-  const model = ai.chat(AI_MODEL);
-  const result = await streamText({
-    model,
-    system: `You are a helpful stock analyst. Answer my questions about the stock market. please answer in ${LANGUAGE_MAP[session?.user.language ?? "ko"]}.`,
-    tools: {
-      getStockInformation: tool({
-        description: "Get the stock information from yahoo finance",
-        parameters: z.object({
-          description: z.string().describe("introduce this stock"),
-          stock: z.string().describe("It's a symbol code of the stock example: AAPL"),
+            if (!quoteSummary || !description) {
+              throw new Error("No data found in the ai tool");
+            }
+
+            return {
+              stockSymbolCode,
+              quoteSummary,
+              description,
+            };
+          },
         }),
-        execute: async ({ stock, description }) => {
-          const quoteSummary = await yahooFinance.quoteSummary(stock);
+      },
+      messages,
+    });
 
-          return {
-            stock,
-            description,
-            quoteSummary,
-          };
-        },
-      }),
-    },
-    messages,
-  });
-
-  return result.toAIStreamResponse();
+    return result.toAIStreamResponse();
+  } catch (error) {
+    console.error(error);
+  }
 };
