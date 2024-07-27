345a51aedcd97d4737a268414f72396002cf8b02
yoonyesol <maniac970430@naver.com>
2024-07-28 04:38:29 +0900

    fix: 서버 컴포넌트에서 다국어 번역 작업이 이루어지지 않는 버그 수정

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,23 @@
 // next-intl
 import { getRequestConfig } from "next-intl/server";
+import { headers } from "next/headers";
+
+function getBasePath(path: string) {
+  const regex = /^(\/[^\/]+)(\/.*)/;
+  const match = path.match(regex);
+  if (match) {
+    return match[1].substring(1);;
+  }
+  return null;
+}
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  const locale = "ko";
+  const headersList = headers();
+  const headerPathname = headersList.get("x-pathname") || "";
+
+  const locale = getBasePath(headerPathname) + "";
 
   return {
     locale,
