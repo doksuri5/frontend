f1ee190f78c010a34aed5088111b019b3e610cd4
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-28 23:49:47 +0900

    refactor: 더보기 버튼 커스텀 훅 생성 및 적용

--- src/app/[lang]/(discovery)/discovery/[search]/_components/DiscoveryNews.tsx
+++ src/app/[lang]/(discovery)/discovery/[search]/_components/DiscoveryNews.tsx
@@ -1,60 +1,46 @@
 "use client";
 
-import { useMemo, useState } from "react";
 import { useTranslations } from "next-intl";
 import { useSession } from "next-auth/react";
 import { FindNews } from "@/components/common";
 import { SearchStockNewsDataType } from "@/types/NewsDataType2";
+import { useShowMoreItems } from "../_hooks/useShowMoreItems";
 
 const DiscoveryNews = ({ newsList }: { newsList: SearchStockNewsDataType[] }) => {
   const { data: session } = useSession();
   const lang = session?.user.language || "ko";
 
   const t = useTranslations();
-  const [showMoreItems, setShowMoreItems] = useState(false);
-  const [maxDisplayedItems, setMaxDisplayedItems] = useState(6);
-
-  const handleShowMore = () => {
-    const nextItemLength = maxDisplayedItems + 6;
-    setMaxDisplayedItems(nextItemLength);
-
-    if (nextItemLength >= newsList.length) setShowMoreItems(false);
-  };
-
-  const displayedStockNews = useMemo(() => {
-    return showMoreItems ? newsList : newsList.slice(0, maxDisplayedItems);
-  }, [newsList, showMoreItems, maxDisplayedItems]);
+  const { displayedItems, showMoreItems, maxDisplayedItems, handleShowMore } = useShowMoreItems(6, newsList);
 
   return (
     <div className="flex_col h-vh gap-[1.6rem] rounded-[1.6rem] bg-white p-[2.4rem]">
-      <>
-        {displayedStockNews.map((news) => {
-          const title = lang && news.title[lang as keyof typeof news.title];
-          const publisher = lang && news.publisher[lang as keyof typeof news.publisher];
-          return (
-            <FindNews
-              key={news.index}
-              _id={news.index}
-              image={news.thumbnailUrl}
-              title={title}
-              publishedTime={news.publishedTime}
-              newspaperCompany={publisher}
-              lang={lang}
-            />
-          );
-        })}
-        {newsList.length > maxDisplayedItems && !showMoreItems && (
-          <>
-            <hr className="mb-[1.6rem] mt-[1.8rem]" />
-            <p
-              className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400"
-              onClick={handleShowMore}
-            >
-              {t("discovery.moreItem")}
-            </p>
-          </>
-        )}
-      </>
+      {displayedItems.map((news) => {
+        const title = lang && news.title[lang as keyof typeof news.title];
+        const publisher = lang && news.publisher[lang as keyof typeof news.publisher];
+        return (
+          <FindNews
+            key={news.index}
+            _id={news.index}
+            image={news.thumbnailUrl}
+            title={title}
+            publishedTime={news.publishedTime}
+            newspaperCompany={publisher}
+            lang={lang}
+          />
+        );
+      })}
+      {newsList.length > maxDisplayedItems && !showMoreItems && (
+        <>
+          <hr className="mb-[1.6rem] mt-[1.8rem]" />
+          <p
+            className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400"
+            onClick={handleShowMore}
+          >
+            {t("discovery.moreItem")}
+          </p>
+        </>
+      )}
     </div>
   );
 };
--- src/app/[lang]/(discovery)/discovery/[search]/_components/DiscoveryStocks.tsx
+++ src/app/[lang]/(discovery)/discovery/[search]/_components/DiscoveryStocks.tsx
@@ -1,44 +1,32 @@
 "use client";
 
-import { useMemo, useState } from "react";
 import { useTranslations } from "next-intl";
 import { StockItem } from "@/components/common";
 import { StockDataType } from "@/types";
+import { useShowMoreItems } from "../_hooks/useShowMoreItems";
 
 const DiscoveryStocks = ({ stockList }: { stockList: StockDataType[] }) => {
   const t = useTranslations();
-  const [showMoreItems, setShowMoreItems] = useState(false);
-  const [maxDisplayedItems, setMaxDisplayedItems] = useState(4);
-
-  const handleShowMore = () => {
-    setShowMoreItems(true);
-    setMaxDisplayedItems(stockList.length);
-  };
-
-  const displayedStockList = useMemo(() => {
-    return showMoreItems ? stockList : stockList.slice(0, maxDisplayedItems);
-  }, [stockList, showMoreItems, maxDisplayedItems]);
+  const { displayedItems, showMoreItems, maxDisplayedItems, handleShowMore } = useShowMoreItems(4, stockList);
 
   return (
     <div className="flex_col rounded-[1.6rem] bg-white p-[2.4rem]">
-      <>
-        <div className="grid w-full grid-cols-2 gap-x-[1.6rem] gap-y-[.8rem]">
-          {displayedStockList.map((stock: StockDataType) => (
-            <StockItem key={stock.stockName} variant="findStock" {...stock} />
-          ))}
-        </div>
-        {stockList.length > maxDisplayedItems && (
-          <>
-            <hr className="mb-[1.6rem] mt-[1.8rem]" />
-            <p
-              className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400"
-              onClick={handleShowMore}
-            >
-              {t("discovery.moreItem")}
-            </p>
-          </>
-        )}
-      </>
+      <div className="grid w-full grid-cols-2 gap-x-[1.6rem] gap-y-[.8rem]">
+        {displayedItems.map((stock: StockDataType) => (
+          <StockItem key={stock.stockName} variant="findStock" {...stock} />
+        ))}
+      </div>
+      {stockList.length > maxDisplayedItems && !showMoreItems && (
+        <>
+          <hr className="mb-[1.6rem] mt-[1.8rem]" />
+          <p
+            className="body_4 w-full cursor-pointer px-[1rem] text-center font-medium text-grayscale-400"
+            onClick={handleShowMore}
+          >
+            {t("discovery.moreItem")}
+          </p>
+        </>
+      )}
     </div>
   );
 };
