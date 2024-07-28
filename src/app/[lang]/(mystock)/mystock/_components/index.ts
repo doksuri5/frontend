861a118036d39023afb85832e966a3677f377893
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-29 01:56:04 +0900

    refactor: 관심 주식 페이지 헤더 부분 컴포넌트 분리

--- src/app/[lang]/(mystock)/mystock/_components/index.ts
+++ src/app/[lang]/(mystock)/mystock/_components/index.ts
@@ -6,6 +6,7 @@ import SearchItem from "./SearchItem";
 import MyStockBody from "./MyStockBody";
 import MyStockPopularSearches from "./MyStockPopularSearches";
 import MyStockRecentSearches from "./MyStockRecentSearches";
+import ModalButton from './ModalButton';
 
 export {
   MyStockHeader,
@@ -16,4 +17,5 @@ export {
   SearchItem,
   MyStockPopularSearches,
   MyStockRecentSearches,
+  ModalButton
 };
