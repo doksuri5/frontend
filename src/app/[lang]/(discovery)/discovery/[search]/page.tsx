133524eee12f309c45a3ac34e8ef338ee64973bf
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-28 23:52:27 +0900

    refactor: 사용하지 않는 코드(revalidatePath) 삭제

--- src/app/[lang]/(discovery)/discovery/[search]/page.tsx
+++ src/app/[lang]/(discovery)/discovery/[search]/page.tsx
@@ -1,4 +1,3 @@
-import { revalidatePath } from "next/cache";
 import { DiscoveryNews, DiscoveryStocks } from "./_components";
 import { DiscoverySection } from "../_components";
 import { saveRecentSearch } from "@/actions/stock";
@@ -8,15 +7,11 @@ import { StockDataType } from "@/types";
 import { SearchStockNewsDataType } from "@/types/NewsDataType2";
 
 export default async function page({ params }: { params: { search: string } }) {
-  const [stockListResult, newsListResult, saveRecentSearchResult] = await Promise.allSettled([
+  const [stockListResult, newsListResult] = await Promise.allSettled([
     getSearchStocks(undefined, { params: decodeURIComponent(params.search) }),
     getSearchNews(undefined, { params: decodeURIComponent(params.search) }),
     saveRecentSearch({ stockName: decodeURIComponent(params.search) }),
   ]);
-
-  if (saveRecentSearchResult.status === "fulfilled" && saveRecentSearchResult.value.ok) {
-    revalidatePath("/discovery");
-  }
 
   const stockList: StockDataType[] =
     stockListResult.status === "fulfilled" && stockListResult.value.ok ? stockListResult.value.data : [];
