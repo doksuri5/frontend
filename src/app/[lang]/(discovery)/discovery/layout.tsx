c7dbcf6ad3c0d3f0bfdfca88f2fe1e37cc830886
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-28 23:48:44 +0900

    refactor: 발견 페이지 레이아웃 Loading 추가

--- src/app/[lang]/(discovery)/discovery/layout.tsx
+++ src/app/[lang]/(discovery)/discovery/layout.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
 import { DiscoveryInput } from "./_components";
+import Loading from "@/app/[lang]/loading";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <article className="flex_col m-auto max-w-[59rem] gap-[3.2rem] py-[5.6rem]">
-      <DiscoveryInput />
-      {children}
-    </article>
+    <Suspense fallback={<Loading />}>
+      <article className="flex_col m-auto max-w-[59rem] gap-[3.2rem] py-[5.6rem]">
+        <DiscoveryInput />
+        {children}
+      </article>
+    </Suspense>
   );
 }
