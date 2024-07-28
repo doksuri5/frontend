c52761d43b67419836557714345e5e54c4f3a80c
magrowing <kmagrowing@gmail.com>
2024-07-29 05:05:11 +0900

    feat : 페이지별 메타데이터 생성 및 적용

--- src/app/[lang]/(auth)/register-complete/page.tsx
+++ src/app/[lang]/(auth)/register-complete/page.tsx
@@ -1,14 +1,17 @@
+import { useTranslations } from "next-intl";
+
 import CommonLayout from "../_components/CommonLayout";
 import CommonLoginBtn from "../_components/CommonLoginBtn";
 
 export default function RegisterCompletePage() {
+  const t = useTranslations("auth");
   return (
     <>
-      <CommonLayout title="가입이 완료되었습니다.">
+      <CommonLayout title={t("title.registrationComplete", { defaultMessage: "가입이 완료되었습니다." })}>
         <p className="body_2 text-center text-grayscale-900">
-          회원가입이 완료되었습니다.
+          {t("registerComplete.registrationComplete", { defaultMessage: "회원가입이 완료되었습니다." })}
           <br />
-          로그인 후 이용해주세요!
+          {t("registerComplete.loginToContinue", { defaultMessage: "로그인 후 이용해주세요!" })}
         </p>
         <CommonLoginBtn className="mt-[5.6rem]" />
       </CommonLayout>
