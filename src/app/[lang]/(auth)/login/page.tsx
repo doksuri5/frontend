4bf0557257b346421a07b3e21742ac66696de462
magrowing <kmagrowing@gmail.com>
2024-07-29 01:31:54 +0900

    feat : 로그인 폼 제외 영역

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 import CommonLayout from "../_components/CommonLayout";
 import LoginForm from "../_components/LoginForm";
@@ -9,15 +10,18 @@ import { cn } from "@/utils/cn";
 import { ACCOUNT_PATH } from "@/routes/path";
 
 export default function LoginPage() {
+  const t = useTranslations("auth");
   return (
     <>
       <CommonLayout title="로그인">
         <LoginForm />
         <dl className={cn("flex_row justify-between")}>
-          <dt className={cn("body_5 text-grayscale-900")}>아직 회원이 아니신가요?</dt>
+          <dt className={cn("body_5 text-grayscale-900")}>
+            {t("login.notMemberYet", { defaultMessage: "아직 회원이 아니신가요?" })}
+          </dt>
           <dd>
             <Link href={ACCOUNT_PATH} className={cn("body_5 block py-[1.6rem] font-medium text-blue-600 underline")}>
-              아잇나우 회원가입
+              {t("login.signup", { defaultMessage: "아잇나우 회원가입" })}
             </Link>
           </dd>
         </dl>
