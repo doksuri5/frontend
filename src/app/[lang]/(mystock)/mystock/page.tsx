b6af81c9380716ac2f55c91859d9196edd37c810
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-30 03:28:58 +0900

    refactor: 동적 import 적용

--- src/app/[lang]/(mystock)/mystock/page.tsx
+++ src/app/[lang]/(mystock)/mystock/page.tsx
@@ -1,16 +1,15 @@
 import dynamic from "next/dynamic";
-import { MyStockHeader } from "@/app/[lang]/(mystock)/mystock/_components";
-import MyStockBodySkeleton from "./_components/_skeleton/MyStockBodySkeleton";
+import { MyStockBodySkeleton, MyStockHeader } from "@/app/[lang]/(mystock)/mystock/_components";
 import { getDetailInterestStocks } from "@/actions/stock";
 import { StockDataType } from "@/types";
 import { auth } from "@/auth";
 
 const MyStockBody = dynamic(() => import("@/app/[lang]/(mystock)/mystock/_components/MyStockBody"), {
-  ssr: false,
+  ssr: true,
   loading: () => <MyStockBodySkeleton />,
 });
 
-const MyStockModalServer = dynamic(() => import("@/app/[lang]/(mystock)/mystock/_components/MyStockModalServer"), {
+const ModalContainer = dynamic(() => import("@/app/[lang]/(mystock)/mystock/_components/_modal/ModalContainer"), {
   ssr: false,
 });
 
@@ -20,13 +19,11 @@ export default async function MyStockPage() {
   const stocksDataResult = await getDetailInterestStocks();
   const stocksData: StockDataType[] = stocksDataResult.ok ? stocksDataResult.data : [];
 
-  const isData = stocksData.length === 0;
-
   return (
     <>
       <MyStockHeader userName={session?.user.name ?? "김스팩"} />
       <MyStockBody dataList={stocksData} />
-      <MyStockModalServer isData={isData} />
+      <ModalContainer isData={stocksData.length === 0} />
     </>
   );
 }
