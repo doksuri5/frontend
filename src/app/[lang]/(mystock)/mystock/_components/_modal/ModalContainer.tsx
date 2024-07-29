e97c480703860b6fdd064c5e38d3002b4bd79c6b
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-07-30 03:31:21 +0900

    refactor: 모달 컴포넌트 구조 수정

--- src/app/[lang]/(mystock)/mystock/_components/_modal/ModalContainer.tsx
+++ src/app/[lang]/(mystock)/mystock/_components/_modal/ModalContainer.tsx
@@ -1,40 +1,95 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState, lazy } from "react";
 import { useTranslations } from "next-intl";
 import { Modal } from "@/components/common";
-import ModalInput from "./ModalInput";
+import {
+  ModalInput,
+  ModalSearchResult,
+  MyStockPopularStockSkeleton,
+  MyStockRecentSearchSkeleton,
+} from "@/app/[lang]/(mystock)/mystock/_components";
 import { useMyStockStore } from "@/providers/MyStockProvider";
-import { StockDataType } from "@/types";
+import { getPopularSearches, getRecentSearchDetails } from "@/actions/stock";
+import { StockDataType, StockPopularSearchDataType } from "@/types/StockDataType";
+
+const ModalPopularSearches = lazy(
+  () => import("@/app/[lang]/(mystock)/mystock/_components/_modal/ModalPopularSearches"),
+);
+const ModalRecentSearches = lazy(() => import("@/app/[lang]/(mystock)/mystock/_components/_modal/ModalRecentSearches"));
 
 const ModalContainer = ({ isData }: { isData: boolean }) => {
   const t = useTranslations("myStock");
+
+  const [popularData, setPopularData] = useState<StockPopularSearchDataType[]>([]);
+  const [recentSearchedData, setRecentSearchedData] = useState<StockDataType[]>([]);
+  const [searchText, setSearchText] = useState("");
 
   const { openModal, setOpenModal } = useMyStockStore((state) => ({
     openModal: state.openModal,
     setOpenModal: state.setOpenModal,
   }));
 
-  const [searchStock, setSearchStock] = useState<StockDataType[]>([]);
+  const closeModal = () => {
+    setOpenModal(false);
+    setSearchText("");
+  };
 
   useEffect(() => {
     isData && setOpenModal(true);
-  }, [isData]);
+  }, [isData, setOpenModal]);
 
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
+  useEffect(() => {
+    if (openModal) {
+      const fetchData = async () => {
+        const [popularDataResult, recentSearchedDataResult] = await Promise.allSettled([
+          getPopularSearches(),
+          getRecentSearchDetails(),
+        ]);
+
+        if (popularDataResult.status === "fulfilled" && popularDataResult.value.ok) {
+          setPopularData(popularDataResult.value.data);
+        }
+
+        if (recentSearchedDataResult.status === "fulfilled" && recentSearchedDataResult.value.ok) {
+          setRecentSearchedData(recentSearchedDataResult.value.data);
+        }
+      };
+
+      fetchData();
+    }
+  }, [openModal]);
 
   return (
     <Modal
       panelStyle="p-[4rem] rounded-[3.2rem] w-[80rem] min-h-[57rem]"
       isOpen={openModal}
-      onClose={handleCloseModal}
+      onClose={closeModal}
       title={t("modal.modalTitle")}
       titleStyle="body_1 text-navy-900"
       closeIcon={true}
     >
-      <ModalInput setSearchStock={setSearchStock} />
+      <div className="mt-[3rem] flex w-full flex-col gap-[2.4rem]">
+        <ModalInput setRecentSearchedData={setRecentSearchedData} setSearchText={setSearchText} />
+        {searchText !== "" ? (
+          <>
+            <ModalSearchResult searchText={searchText} />
+          </>
+        ) : (
+          <>
+            <Suspense fallback={<MyStockRecentSearchSkeleton />}>
+              <ModalRecentSearches
+                recentSearchedData={recentSearchedData}
+                setRecentSearchedData={setRecentSearchedData}
+                setSearchText={setSearchText}
+              />
+            </Suspense>
+            <Suspense fallback={<MyStockPopularStockSkeleton />}>
+              <ModalPopularSearches popularData={popularData} />
+            </Suspense>
+          </>
+        )}
+      </div>
     </Modal>
   );
 };
