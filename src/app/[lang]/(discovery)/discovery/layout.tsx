d6747b27333f799ec71c9085db4e128fab01857a
knoa0405 <knoa0405@gmail.com>
2024-07-31 03:26:16 +0900

    fix: error in static rendering build

--- src/app/[lang]/(discovery)/discovery/layout.tsx
+++ src/app/[lang]/(discovery)/discovery/layout.tsx
@@ -1,8 +1,11 @@
 import { Suspense } from "react";
 import { DiscoveryInput } from "./_components";
 import Loading from "@/app/[lang]/loading";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default function layout({ children, params }: { children: React.ReactNode; params: { lang: string } }) {
+  unstable_setRequestLocale(params.lang);
+
   return (
     <Suspense fallback={<Loading />}>
       <article className="flex_col m-auto max-w-[59rem] gap-[3.2rem] py-[5.6rem]">
