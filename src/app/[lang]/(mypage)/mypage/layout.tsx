6e4bf1921f887bd42b74c2f5b02aed299ae7b3d5
yoonyesol <maniac970430@naver.com>
2024-07-31 01:13:03 +0900

    feat: 마이페이지 메타태그 적용

--- src/app/[lang]/(mypage)/mypage/layout.tsx
+++ src/app/[lang]/(mypage)/mypage/layout.tsx
@@ -1,5 +1,34 @@
+import Head from 'next/head';
 import { Suspense, lazy } from "react";
 import { useTranslations } from "next-intl";
+
+interface IMetadata {
+  title: string;
+  description: string;
+  keywords: string;
+  openGraph: {
+    title: string;
+    description: string;
+  };
+  twitter: {
+    title: string;
+    description: string;
+  };
+}
+
+export const metadata: IMetadata = {
+  title: "마이페이지",
+  description: "사용자의 개인 설정과 정보를 관리할 수 있는 마이 페이지입니다. 개인 설정과 정보를 쉽게 관리하세요.",
+  keywords: "마이페이지, 언어 설정, 개인정보 수정, 개인정보, 프로필, 회원탈퇴, 탈퇴, 이용약관, 약관",
+  openGraph: {
+    title: '마이페이지',
+    description: "사용자의 개인 설정과 정보를 관리할 수 있는 마이 페이지입니다. 개인 설정과 정보를 쉽게 관리하세요."
+  },
+  twitter: {
+    title: "마이페이지",
+    description: "사용자의 개인 설정과 정보를 관리할 수 있는 마이 페이지입니다. 개인 설정과 정보를 쉽게 관리하세요."
+  }
+}
 
 const Navigation = lazy(() => import("./_components/Navigation"));
 
@@ -8,6 +37,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="twitter:title" content={metadata.twitter.title} />
+        <meta property="twitter:description" content={metadata.twitter.description} />
+      </Head>
+
       <header className="heading_4 pb-[2rem] pt-[5.6rem] font-bold text-gray-900">{t("myPage")}</header>
       <div className="flex flex-row gap-[2.7rem] pb-[11.2rem]">
         <aside className="min-h-[72rem] min-w-[28.5rem] rounded-[1.6rem] bg-grayscale-0">
