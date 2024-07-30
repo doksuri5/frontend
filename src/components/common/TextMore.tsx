d8b5c6befba91bf6b8e75506550ce610f6780ca0
knoa0405 <knoa0405@gmail.com>
2024-07-30 18:33:49 +0900

    feat: 더보기 텍스트 다국어 추가

--- src/components/common/TextMore.tsx
+++ src/components/common/TextMore.tsx
@@ -2,8 +2,10 @@
 
 import { useReportStore } from "@/providers/ReportProvider";
 import { useState, useRef, useEffect, ReactNode } from "react";
+import { useTranslations } from "use-intl";
 
 const TextMore = ({ children }: { children: ReactNode }) => {
+  const t = useTranslations("report");
   const setIsExtended = useReportStore((state) => state.setIsExtended);
 
   const [moreText, setMoreText] = useState(false);
@@ -39,7 +41,7 @@ const TextMore = ({ children }: { children: ReactNode }) => {
       </p>
       {overFlow && (
         <button className="body_5 absolute bottom-[-10] right-0 bg-white font-semibold" onClick={toggleMoreText}>
-          {moreText ? "접기" : "더보기"}
+          {moreText ? t("less") : t("more")}
         </button>
       )}
     </div>
