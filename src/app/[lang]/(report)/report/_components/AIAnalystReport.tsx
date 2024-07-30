a0e69ff571eddc35746f4dfd9cc7664953e91423
knoa0405 <knoa0405@gmail.com>
2024-07-30 13:04:30 +0900

    refactor: ai report 컴포넌트 서버 컴포넌트로 변경

--- src/app/[lang]/(report)/report/_components/AIAnalystReport.tsx
+++ src/app/[lang]/(report)/report/_components/AIAnalystReport.tsx
@@ -1,34 +1,19 @@
-"use client";
-
 import { getStockAnalysis } from "@/actions/stock-analysis";
 import { STOCK_NAMES, TReutersCodes } from "@/constants/stockCodes";
-import { StockAIReportDataType } from "@/types/StockDataType";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import AIAnalystReportSkeleton from "./skeleton/AIAnalystReportSkeleton";
 import StockPrice from "@/components/common/StockPrice";
+import { auth } from "@/auth";
 
 type TAIAnalystReport = {
   reutersCode: TReutersCodes;
 };
 
-export default function AIAnalystReport({ reutersCode }: TAIAnalystReport) {
-  const [aiData, setAiData] = useState<StockAIReportDataType>();
+const AIAnalystReport = async ({ reutersCode }: TAIAnalystReport) => {
+  const session = await auth();
+  const data = await getStockAnalysis(undefined, { params: reutersCode });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getStockAnalysis(undefined, { params: reutersCode });
-      if (!data.data[0]) {
-        return;
-      }
-
-      setAiData(data.data[0]);
-    };
-
-    fetchData();
-  }, [reutersCode]);
-
-  if (!aiData) {
+  if (!data) {
     return <AIAnalystReportSkeleton />;
   }
 
@@ -37,11 +22,13 @@ export default function AIAnalystReport({ reutersCode }: TAIAnalystReport) {
       <h2 className="body_1 pb-[5.5rem] font-bold text-navy-900">아잇나우 AI 애널리스트 리포트</h2>
       <div className="mb-[1.6rem] flex items-center gap-[0.8rem] text-grayscale-900">
         <Image src={`/icons/stocks/${STOCK_NAMES[reutersCode]}.svg`} alt="icon" width={30} height={30} />
-        <h3 className="body_3 font-bold">{aiData.metrics.stockName}</h3>
-        <h3 className="body_3 font-normal">{aiData.metrics.symbolCode}</h3>
+        <h3 className="body_3 font-bold">{data.data[0].metrics.stockName}</h3>
+        <h3 className="body_3 font-normal">{data.data[0].metrics.symbolCode}</h3>
         <StockPrice reutersCode={reutersCode} />
       </div>
-      <p className="body_4 font-medium">{aiData.report}</p>
+      <p className="body_4 font-medium">{data.data[0].report[session?.user.language ?? "ko"]}</p>
     </section>
   );
-}
+};
+
+export default AIAnalystReport;
