6ff48f870b921ad50010e63654514d5b239c1d45
knoa0405 <knoa0405@gmail.com>
2024-07-31 13:07:50 +0900

    feat: 메인페이지 다국어 작업

--- src/app/[lang]/(home)/home/_components/popular-interest-news.tsx
+++ src/app/[lang]/(home)/home/_components/popular-interest-news.tsx
@@ -1,20 +1,22 @@
 import { fetchInterestStockNews, fetchPopularNews } from "@/actions/news";
 import { Card, NewsItem } from "@/components/common";
 import DraggableList from "@/components/common/DraggableList";
+import { getTranslations } from "next-intl/server";
 
 const PopularInterestNews = async () => {
+  const t = await getTranslations("home");
   const interestNews = await fetchInterestStockNews();
   const popularNews = (await fetchPopularNews()) ?? [];
 
   return (
     <>
-      <p className="body_1 pb-[1.6rem]">관심종목</p>
+      <p className="body_1 pb-[1.6rem]">{t("interestStocks")}</p>
       <DraggableList>
         {interestNews.map((item: any) => (
           <Card key={item._id} _id={item._id} variant="iconCard" date={item.date} title={item.title} />
         ))}
       </DraggableList>
-      <p className="body_1 pb-[1.6rem] pt-[4.8rem]">주요 뉴스</p>
+      <p className="body_1 pb-[1.6rem] pt-[4.8rem]">{t("popularNews")}</p>
       <div className="flex flex-col gap-[1.6rem] divide-y rounded-[1.6rem] border p-[4.8rem]">
         <NewsItem
           variant="lineClamp-4"
