418bb5a7d5494e4c150a163e8efc6603caadd693
knoa0405 <knoa0405@gmail.com>
2024-07-31 13:07:50 +0900

    feat: 메인페이지 다국어 작업

--- src/app/[lang]/(home)/home/_components/news-box.tsx
+++ src/app/[lang]/(home)/home/_components/news-box.tsx
@@ -2,13 +2,15 @@ import PopularInterestNews from "./popular-interest-news";
 import { Suspense } from "react";
 import RecentNews from "./recent-news";
 import { auth } from "@/auth";
+import { getTranslations } from "next-intl/server";
 
 const NewsBox = async () => {
   const session = await auth();
+  const t = await getTranslations("home");
 
   return (
     <section>
-      <h2 className="heading_4 pb-[2.4rem] font-bold">{session?.user.name ?? "김스팩"}님을 위한 주식 뉴스</h2>
+      <h2 className="heading_4 pb-[2.4rem] font-bold">{t("newsForUser", { name: session?.user.name ?? "김스팩" })}</h2>
       <div className="flex flex-col rounded-[1.6rem] bg-white p-[4.8rem]">
         <Suspense>
           <PopularInterestNews />
