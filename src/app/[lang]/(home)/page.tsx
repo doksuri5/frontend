2a59e79b38cd2b4d82926f08ddd23c3551e5b0a1
yoonyesol <maniac970430@naver.com>
2024-08-01 11:34:05 +0900

    fix: root 페이지를 비동기 함수로 변경 및 텍스트 정렬 수정

--- src/app/[lang]/(home)/page.tsx
+++ src/app/[lang]/(home)/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import CommonLoginBtn from "../(auth)/_components/CommonLoginBtn";
-import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { unstable_setRequestLocale } from "next-intl/server";
 import { Locale } from "@/i18n";
+import { useTranslations } from "next-intl";
 
-export default async function HomeRootPage({ params }: { params: { lang: Locale } }) {
+export default function HomeRootPage({ params }: { params: { lang: Locale } }) {
   unstable_setRequestLocale(params.lang);
-  const t = await getTranslations("main");
+  const t = useTranslations("root");
 
   return (
     <div className="flex flex-col items-center justify-between">
       <dl className="flex_col_center fadeInUp-animation mb-[5.8rem] mt-[7rem] text-white">
-        <dt className="heading_1 font-medium">
+        <dt className="heading_1 font-medium text-center">
           {t("part1")} <em className="font-extrabold not-italic">{t("part2")}</em>{t("part3")}
         </dt>
         <dd className="heading_4 mb-[5.6rem] mt-[2.4rem] text-center font-medium whitespace-pre-wrap">
